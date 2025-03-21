2f3f0ca2bfd60d4197e197ae3915a96a97d072ca
Eric Le Ponner <eric.leponner@icloud.com>
2025-03-21 14:58:37 +0100

    Added AccountGrowthController, AvgTimeToConsensusController and TransactionCountController.
    Signed-off-by: Eric Le Ponner <eric.leponner@icloud.com>

---
+++ src/charts/hgraph/TransactionCountController.ts
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: Apache-2.0
+
+import {ThemeController} from "@/components/ThemeController.ts";
+import {RouteManager} from "@/utils/RouteManager.ts";
+import {GenericMetricController} from "@/charts/hgraph/GenericMetricController.ts";
+import {EcosystemMetric} from "@/charts/hgraph/EcosystemMetric.ts";
+import {ChartRange} from "@/charts/core/ChartRange.ts";
+import {ChartConfiguration} from "chart.js/auto";
+
+export class TransactionCountController extends GenericMetricController {
+
+    //
+    // Public
+    //
+
+    public constructor(themeController: ThemeController, routeManager: RouteManager) {
+        super("Transactions Over Time", "transactions", themeController, routeManager)
+    }
+
+    //
+    // ChartController
+    //
+
+    protected makeChartConfig(metrics: EcosystemMetric[], range: ChartRange,
+                              context: CanvasRenderingContext2D): ChartConfiguration {
+        return this.makeBarChartConfig(metrics, range, false, "# of accounts", context)
+    }
+}
