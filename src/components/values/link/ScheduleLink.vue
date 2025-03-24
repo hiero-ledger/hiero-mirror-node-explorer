7ed46a05a66bb5e587e8a7f9f9cf9f827181edf7
Simon Viénot <simon.vienot@icloud.com>
2025-03-24 18:15:45 +0100

    Add ScheduleLink component.
    Signed-off-by: Simon Viénot <simon.vienot@icloud.com>

---
+++ src/components/values/link/ScheduleLink.vue
@@ -0,0 +1,43 @@
+// SPDX-License-Identifier: Apache-2.0
+
+<!-- --------------------------------------------------------------------------------------------------------------- -->
+<!--                                                     TEMPLATE                                                    -->
+<!-- --------------------------------------------------------------------------------------------------------------- -->
+
+<template>
+
+  <div class="is-inline-block">
+
+    <EntityLink :route="scheduleRoute">
+      <EntityIOL :entity-id="props.scheduleId"/>
+    </EntityLink>
+
+  </div>
+
+</template>
+
+<!-- --------------------------------------------------------------------------------------------------------------- -->
+<!--                                                      SCRIPT                                                     -->
+<!-- --------------------------------------------------------------------------------------------------------------- -->
+
+<script setup lang="ts">
+
+import {computed} from "vue";
+import {routeManager} from "@/router";
+import EntityLink from "@/components/values/link/EntityLink.vue";
+import EntityIOL from "@/components/values/link/EntityIOL.vue";
+
+const props = defineProps({
+  scheduleId: String,
+})
+
+const scheduleRoute = computed(() => props.scheduleId ? routeManager.makeRouteToSchedule(props.scheduleId) : null)
+
+</script>
+
+<!-- --------------------------------------------------------------------------------------------------------------- -->
+<!--                                                       STYLE                                                     -->
+<!-- --------------------------------------------------------------------------------------------------------------- -->
+
+<style/>
+
