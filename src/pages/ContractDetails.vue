6f1e573c75419229f82c24e8bb0b31e5c96b5367
Simon Viénot <simon.vienot@icloud.com>
2025-04-03 16:47:47 +0200

    Leverage DomainLabel and PublicLabel in ContractDetails.
    Signed-off-by: Simon Viénot <simon.vienot@icloud.com>

--- src/pages/ContractDetails.vue
+++ src/pages/ContractDetails.vue
@@ -59,8 +59,19 @@
             Domain
           </template>
           <template #value>
-            <EntityIOL :label="domainName"/>
-            <InfoTooltip v-if="domainProviderName" :label="domainProviderName"/>
+            <div style="display: flex; align-items: center; gap: 4px">
+              <DomainLabel :domain-name="domainName" :provider-name="domainProviderName"/>
+            </div>
+          </template>
+        </Property>
+        <Property v-if="label" id="labels" full-width>
+          <template #name>
+            Label
+          </template>
+          <template #value>
+            <div style="display: flex; align-items: center; gap: 4px">
+              <PublicLabel :label-definition="label"/>
+            </div>
           </template>
         </Property>
       </template>
@@ -248,8 +259,6 @@ import {ContractResultsLogsAnalyzer} from "@/utils/analyzer/ContractResultsLogsA
 import {BalanceAnalyzer} from "@/utils/analyzer/BalanceAnalyzer";
 import MirrorLink from "@/components/MirrorLink.vue";
 import {NameQuery} from "@/utils/name_service/NameQuery";
-import EntityIOL from "@/components/values/link/EntityIOL.vue";
-import InfoTooltip from "@/components/InfoTooltip.vue";
 import {labelForAutomaticTokenAssociation} from "@/schemas/MirrorNodeUtils.ts";
 import TokensSection from "@/components/token/TokensSection.vue";
 import ContractERCSection from "@/components/contract/ContractERCSection.vue";
@@ -257,6 +266,9 @@ import DashboardCardV2 from "@/components/DashboardCardV2.vue";
 import ArrowLink from "@/components/ArrowLink.vue";
 import EntityIDView from "@/components/values/EntityIDView.vue";
 import HbarAmount from "@/components/values/HbarAmount.vue";
+import DomainLabel from "@/components/values/DomainLabel.vue";
+import PublicLabel from "@/components/values/PublicLabel.vue";
+import {LabelByIdCache} from "@/utils/cache/LabelByIdCache.ts";
 
 const props = defineProps({
   contractId: String,
@@ -344,9 +356,19 @@ onBeforeUnmount(() => contractResultsLogsAnalyzer.unmount())
 //
 // Naming
 //
-const nameQuery = new NameQuery(computed(() => props.contractId ?? null))
+const nameQuery = new NameQuery(normalizedContractId)
 onMounted(() => nameQuery.mount())
 onBeforeUnmount(() => nameQuery.unmount())
+const domainName = nameQuery.name
+const domainProviderName = nameQuery.providerName
+
+//
+// Label
+//
+const labelLookup = LabelByIdCache.instance.makeLookup(normalizedContractId)
+onMounted(() => labelLookup.mount())
+onBeforeUnmount(() => labelLookup.unmount())
+const label = labelLookup.entity
 
 const enableExpiry = routeManager.enableExpiry
 const contract = contractLocParser.entity
@@ -356,8 +378,6 @@ const hbarBalance = balanceAnalyzer.hbarBalance
 const isVerified = contractAnalyzer.isVerified
 const contractName = contractAnalyzer.contractName
 const logs = contractResultsLogsAnalyzer.logs
-const domainName = nameQuery.name
-const domainProviderName = nameQuery.providerName
 
 </script>
 
