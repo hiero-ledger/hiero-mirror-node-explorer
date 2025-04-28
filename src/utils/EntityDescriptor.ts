783f94cfd14ddea3bb4ec7b1e831b51ee927a89c
Simon Viénot <simon.vienot@icloud.com>
2025-04-28 09:56:20 +0200

    Try to protect EntityDescriptor.makeEntityDescriptor against e2e test failure.
    Signed-off-by: Simon Viénot <simon.vienot@icloud.com>

--- src/utils/EntityDescriptor.ts
+++ src/utils/EntityDescriptor.ts
@@ -14,98 +14,101 @@ export class EntityDescriptor {
     ) {
     }
 
-    static async makeEntityDescriptor(row: Transaction): Promise<EntityDescriptor> {
+    static async makeEntityDescriptor(row: Transaction | null): Promise<EntityDescriptor> {
         let result: EntityDescriptor
-        switch (row.name) {
+        if (row !== null) {
+            switch (row.name) {
 
-            case TransactionType.CONSENSUSCREATETOPIC:
-            case TransactionType.CONSENSUSUPDATETOPIC:
-            case TransactionType.CONSENSUSDELETETOPIC:
-            case TransactionType.CONSENSUSSUBMITMESSAGE:
-                result = new EntityDescriptor("Topic ID", "TopicDetails")
-                break;
+                case TransactionType.CONSENSUSCREATETOPIC:
+                case TransactionType.CONSENSUSUPDATETOPIC:
+                case TransactionType.CONSENSUSDELETETOPIC:
+                case TransactionType.CONSENSUSSUBMITMESSAGE:
+                    result = new EntityDescriptor("Topic ID", "TopicDetails")
+                    break;
 
-            case TransactionType.ETHEREUMTRANSACTION:
-                if (row.entity_id && await ContractByIdCache.instance.lookup(row.entity_id)) {
+                case TransactionType.ETHEREUMTRANSACTION:
+                    if (row.entity_id && await ContractByIdCache.instance.lookup(row.entity_id)) {
+                        result = new EntityDescriptor("Contract ID", "ContractDetails")
+                    } else if (row.entity_id && await AccountByIdCache.instance.lookup(row.entity_id)) {
+                        result = new EntityDescriptor("Account ID", "AccountDetails")
+                    } else {
+                        result = new EntityDescriptor("Token ID", "TokenDetails")
+                    }
+                    break;
+
+                case TransactionType.CONTRACTCALL:
+                    if (row.entity_id && await ContractByIdCache.instance.lookup(row.entity_id)) {
+                        result = new EntityDescriptor("Contract ID", "ContractDetails")
+                    } else {
+                        result = new EntityDescriptor("Token ID", "TokenDetails")
+                    }
+                    break;
+
+                case TransactionType.CONTRACTDELETEINSTANCE:
+                case TransactionType.CONTRACTCREATEINSTANCE:
+                case TransactionType.CONTRACTUPDATEINSTANCE:
                     result = new EntityDescriptor("Contract ID", "ContractDetails")
-                } else if (row.entity_id && await AccountByIdCache.instance.lookup(row.entity_id)) {
+                    break;
+
+                case TransactionType.CRYPTOADDLIVEHASH:
+                case TransactionType.CRYPTOCREATEACCOUNT:
+                case TransactionType.CRYPTODELETE:
+                case TransactionType.CRYPTOUPDATEACCOUNT:
+                case TransactionType.CRYPTODELETELIVEHASH:
+                case TransactionType.CRYPTOTRANSFER:
+                case TransactionType.TOKENASSOCIATE:
+                case TransactionType.TOKENDISSOCIATE:
+                case TransactionType.CRYPTOAPPROVEALLOWANCE:
+                case TransactionType.CRYPTODELETEALLOWANCE:
+                case TransactionType.TOKENGRANTKYC:
+                case TransactionType.TOKENREVOKEKYC:
+                case TransactionType.TOKENFREEZE:
+                case TransactionType.TOKENUNFREEZE:
+                case TransactionType.TOKENREJECT:
                     result = new EntityDescriptor("Account ID", "AccountDetails")
-                } else {
-                    result = new EntityDescriptor("Token ID", "TokenDetails")
-                }
-                break;
+                    break;
 
-            case TransactionType.CONTRACTCALL:
-                if (row.entity_id && await ContractByIdCache.instance.lookup(row.entity_id)) {
-                    result = new EntityDescriptor("Contract ID", "ContractDetails")
-                } else {
-                    result = new EntityDescriptor("Token ID", "TokenDetails")
-                }
-                break;
+                case TransactionType.FILECREATE:
+                case TransactionType.FILEDELETE:
+                case TransactionType.FILEAPPEND:
+                case TransactionType.FILEUPDATE:
+                case TransactionType.FREEZE:
+                    result = new EntityDescriptor("File ID", null);
+                    break;
 
-            case TransactionType.CONTRACTDELETEINSTANCE:
-            case TransactionType.CONTRACTCREATEINSTANCE:
-            case TransactionType.CONTRACTUPDATEINSTANCE:
-                result = new EntityDescriptor("Contract ID", "ContractDetails")
-                break;
+                case TransactionType.SCHEDULECREATE:
+                case TransactionType.SCHEDULEDELETE:
+                case TransactionType.SCHEDULESIGN:
+                    result = new EntityDescriptor("Schedule ID", "ScheduleDetails");
+                    break;
 
-            case TransactionType.CRYPTOADDLIVEHASH:
-            case TransactionType.CRYPTOCREATEACCOUNT:
-            case TransactionType.CRYPTODELETE:
-            case TransactionType.CRYPTOUPDATEACCOUNT:
-            case TransactionType.CRYPTODELETELIVEHASH:
-            case TransactionType.CRYPTOTRANSFER:
-            case TransactionType.TOKENASSOCIATE:
-            case TransactionType.TOKENDISSOCIATE:
-            case TransactionType.CRYPTOAPPROVEALLOWANCE:
-            case TransactionType.CRYPTODELETEALLOWANCE:
-            case TransactionType.TOKENGRANTKYC:
-            case TransactionType.TOKENREVOKEKYC:
-            case TransactionType.TOKENFREEZE:
-            case TransactionType.TOKENUNFREEZE:
-            case TransactionType.TOKENREJECT:
-                result = new EntityDescriptor("Account ID", "AccountDetails")
-                break;
+                case TransactionType.TOKENAIRDROP:
+                case TransactionType.TOKENBURN:
+                case TransactionType.TOKENCANCELAIRDROP:
+                case TransactionType.TOKENCLAIMAIRDROP:
+                case TransactionType.TOKENCREATION:
+                case TransactionType.TOKENDELETION:
+                case TransactionType.TOKENUPDATE:
+                case TransactionType.TOKENUPDATENFTS:
+                case TransactionType.TOKENFEESCHEDULEUPDATE:
+                case TransactionType.TOKENMINT:
+                case TransactionType.TOKENPAUSE:
+                case TransactionType.TOKENUNPAUSE:
+                case TransactionType.TOKENWIPE:
+                    result = new EntityDescriptor("Token ID", "TokenDetails");
+                    break;
 
-            case TransactionType.FILECREATE:
-            case TransactionType.FILEDELETE:
-            case TransactionType.FILEAPPEND:
-            case TransactionType.FILEUPDATE:
-            case TransactionType.FREEZE:
-                result = new EntityDescriptor("File ID", null);
-                break;
-
-            case TransactionType.SCHEDULECREATE:
-            case TransactionType.SCHEDULEDELETE:
-            case TransactionType.SCHEDULESIGN:
-                result = new EntityDescriptor("Schedule ID", "ScheduleDetails");
-                break;
-
-            case TransactionType.TOKENAIRDROP:
-            case TransactionType.TOKENBURN:
-            case TransactionType.TOKENCANCELAIRDROP:
-            case TransactionType.TOKENCLAIMAIRDROP:
-            case TransactionType.TOKENCREATION:
-            case TransactionType.TOKENDELETION:
-            case TransactionType.TOKENUPDATE:
-            case TransactionType.TOKENUPDATENFTS:
-            case TransactionType.TOKENFEESCHEDULEUPDATE:
-            case TransactionType.TOKENMINT:
-            case TransactionType.TOKENPAUSE:
-            case TransactionType.TOKENUNPAUSE:
-            case TransactionType.TOKENWIPE:
-                result = new EntityDescriptor("Token ID", "TokenDetails");
-                break;
-
-            case TransactionType.ATOMICBATCH:
-            case TransactionType.SYSTEMDELETE:
-            case TransactionType.SYSTEMUNDELETE:
-            case TransactionType.UNCHECKEDSUBMIT:
-            default:
-                result = EntityDescriptor.DEFAULT_ENTITY_DESCRIPTOR;
-                break;
+                case TransactionType.ATOMICBATCH:
+                case TransactionType.SYSTEMDELETE:
+                case TransactionType.SYSTEMUNDELETE:
+                case TransactionType.UNCHECKEDSUBMIT:
+                default:
+                    result = EntityDescriptor.DEFAULT_ENTITY_DESCRIPTOR;
+                    break;
+            }
+        } else {
+            result = EntityDescriptor.DEFAULT_ENTITY_DESCRIPTOR;
         }
-
         return Promise.resolve(result)
     }
 }
