eca414740fc3805eedf2f654f7e46eb92a9335b2
Eric Le Ponner <eric.leponner@icloud.com>
2025-05-06 16:10:01 +0200

    Added ProfileTabItem and Profile page.
    Signed-off-by: Eric Le Ponner <eric.leponner@icloud.com>

---
+++ src/pages/Profile.vue
@@ -0,0 +1,59 @@
+/ SPDX-License-Identifier: Apache-2.0
+
+<!-- --------------------------------------------------------------------------------------------------------------- -->
+<!--                                                     TEMPLATE                                                    -->
+<!-- --------------------------------------------------------------------------------------------------------------- -->
+
+<template>
+
+  <PageFrameV2 page-title="Profile">
+
+    <div class="profile">
+      <template v-if="connectionStatus == ProfileConnectionStatus.Disconnected">
+        <p>Disconnected</p>
+        <ButtonView @action="profileController.connect()">Connect</ButtonView>
+      </template>
+      <template v-else-if="connectionStatus == ProfileConnectionStatus.Connecting">
+        <p>Connecting…</p>
+      </template>
+      <template v-else>
+        <p>Connected</p>
+        <ButtonView @action="profileController.disconnect()">Disconnect</ButtonView>
+      </template>
+    </div>
+
+  </PageFrameV2>
+
+</template>
+
+<!-- --------------------------------------------------------------------------------------------------------------- -->
+<!--                                                      SCRIPT                                                     -->
+<!-- --------------------------------------------------------------------------------------------------------------- -->
+
+<script setup lang="ts">
+
+import PageFrameV2 from "@/components/page/PageFrameV2.vue";
+import {ProfileConnectionStatus, ProfileController} from "@/utils/profile/ProfileController.ts";
+import ButtonView from "@/elements/ButtonView.vue";
+
+const profileController = ProfileController.inject()
+const connectionStatus = profileController.connectionStatus
+
+</script>
+
+<!-- --------------------------------------------------------------------------------------------------------------- -->
+<!--                                                       STYLE                                                     -->
+<!-- --------------------------------------------------------------------------------------------------------------- -->
+
+<style scoped>
+
+div.profile {
+  color: var(--text-secondary);
+  min-height: 450px;
+  text-align: center;
+  padding-top: 100px;
+  font-family: var(--font-family-proportional), sans-serif;
+  font-weight: 300;;
+}
+
+</style>
