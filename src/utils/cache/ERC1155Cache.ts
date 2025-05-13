4c15d4f9ee1b0054994121c26f20860566ab81ad
Simon Viénot <simon.vienot@icloud.com>
2025-05-13 14:36:30 +0200

    ERC1155Cache is now a SingletonCache instead of a EntityCache.
    Signed-off-by: Simon Viénot <simon.vienot@icloud.com>

--- src/utils/cache/ERC1155Cache.ts
+++ src/utils/cache/ERC1155Cache.ts
@@ -1,57 +1,49 @@
 // SPDX-License-Identifier: Apache-2.0
 
-import {EntityCache} from "@/utils/cache/base/EntityCache.ts";
-import {routeManager} from "@/router.ts";
+import {SingletonCache} from "@/utils/cache/base/SingletonCache";
 import axios from "axios";
+import {routeManager} from "@/router";
 
-export class ERC1155Cache extends EntityCache<string, ERC1155Contract | null> {
-
-    private contracts: ERC1155Contract[] = [];
-    private loaded = false;
-
-    //
-    // Public
-    //
+export class ERC1155Cache extends SingletonCache<ERC1155Index> {
 
     public static readonly instance = new ERC1155Cache()
 
     //
-    // EntityCache
+    // Cache
     //
 
-    protected async load(key: string): Promise<ERC1155Contract | null> {
-        let result: ERC1155Contract | null = null
-
-        if (!this.loaded) {
-            this.loaded = true
-            await this.loadERC1155Contracts()
-        }
-        for (const contract of this.contracts) {
-            if (contract.contractId === key) {
-                result = contract
-                break
-            }
-        }
-        return Promise.resolve(result)
-    }
-
-    public override clear(): void {
-        super.clear()
-        this.loaded = false
-    }
-
-    //
-    // Private
-    //
-
-    private async loadERC1155Contracts(): Promise<void> {
+    protected async load(): Promise<ERC1155Index> {
+        let contracts: ERC1155Contract[]
         const url = routeManager.currentNetworkEntry.value.erc1155IndexURL
         if (url !== null) {
-            this.contracts = (await axios.get<ERC1155Contract[]>(url)).data
+            const response = await axios.get<ERC1155Contract[]>(url)
+            if (response.status === 200 && typeof response.data === "object" && response.data !== null) {
+                contracts = response.data
+                console.log(`Read ${contracts.length} contracts from url: ${url}`)
+            } else {
+                contracts = []
+                console.warn(`Could not read contracts from url: ${url}`)
+            }
         } else {
-            this.contracts = []
+            contracts = []
         }
-        return Promise.resolve()
+        return new ERC1155Index(contracts)
+    }
+}
+
+export class ERC1155Index {
+
+    constructor(private readonly contracts: ERC1155Contract[]) {
+    }
+
+    public lookup(contractId: string): ERC1155Contract | null {
+        let result: ERC1155Contract | null = null
+        for (const c of this.contracts) {
+            if (c.contractId === contractId) {
+                result = c
+            }
+        }
+        return result
     }
 }
 
