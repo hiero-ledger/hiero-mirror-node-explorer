4b1e2be2a2d8dd27cbfebcffc8105fd6e5455082
Eric Le Ponner <eric.leponner@icloud.com>
2025-05-14 15:27:50 +0200

    Moved some template part from Profile to ProfileContent vue.
    Signed-off-by: Eric Le Ponner <eric.leponner@icloud.com>

--- src/pages/Profile.vue
+++ src/pages/Profile.vue
@@ -35,133 +35,7 @@
 
     <template v-else>
 
-      <DashboardCardV2 collapsible-key="profileDetails">
-
-        <template #title>
-          Profile
-        </template>
-
-        <template #right-control>
-          <ButtonView
-              id="disconnect-profile-button"
-              :enabled="connectionStatus == ProfileConnectionStatus.Connected"
-              :size="ButtonSize.small"
-              @action="profileController.disconnect()"
-          >
-            <span>DISCONNECT</span>
-          </ButtonView>
-        </template>
-
-        <template #left-content>
-
-          <Property id="firstName" full-width>
-            <template #name>e-Mail</template>
-            <template v-slot:value>
-              <StringValue :string-value="email"/>
-            </template>
-          </Property>
-
-          <Property id="firstName" full-width>
-            <template #name>First Name</template>
-            <template v-slot:value>
-              <StringValue :string-value="firstName"/>
-            </template>
-          </Property>
-
-          <Property id="firstName" full-width>
-            <template #name>Last Name</template>
-            <template v-slot:value>
-              <StringValue :string-value="lastName"/>
-            </template>
-          </Property>
-
-
-        </template>
-
-        <template #right-content>
-
-          <Property id="ecdsa-account-id" full-width>
-            <template #name>ECDSA Account ID</template>
-            <template v-slot:value>
-              <AccountLink :account-id="ecdsaAccountId"/>
-            </template>
-          </Property>
-
-          <Property id="firstName" full-width>
-            <template #name>ED25519 Account ID</template>
-            <template v-slot:value>
-              <AccountLink :account-id="ed25519AccountId"/>
-            </template>
-          </Property>
-
-        </template>
-
-      </DashboardCardV2>
-
-      <DashboardCardV2 collapsible-key="bookmarks">
-
-        <template #title>
-          Bookmarks
-        </template>
-
-        <template #right-control>
-          <ButtonView
-              id="add-bookmark-button"
-              :enabled="connectionStatus == ProfileConnectionStatus.Connected"
-              :size="ButtonSize.small"
-              @action="handleAddBookmark"
-          >
-            <span>NEW BOOKMARK</span>
-          </ButtonView>
-        </template>
-
-        <template #content>
-
-          <o-table
-              :data="bookmarks"
-              :hoverable="true"
-              :mobile-breakpoint="ORUGA_MOBILE_BREAKPOINT"
-              :narrowed="true"
-              :paginated="bookmarks.length > perPage"
-              pagination-order="centered"
-              :range-before="1"
-              :range-after="1"
-
-              :per-page="perPage"
-              :striped="true"
-              aria-current-label="Current page"
-
-              aria-next-label="Next page"
-              aria-page-label="Page"
-              aria-previous-label="Previous page"
-              customRowKey="contract_id"
-          >
-
-            <o-table-column v-slot="props" field="name" label="NAME">
-              <StringValue :string-value="props.row.name"/>
-            </o-table-column>
-
-            <o-table-column v-slot="props" field="entityId" label="ID">
-              <StringValue :string-value="props.row.entityId"/>
-            </o-table-column>
-
-            <o-table-column v-slot="props" field="entityType" label="ENTITY">
-              <StringValue :string-value="props.row.entityType"/>
-            </o-table-column>
-
-            <o-table-column v-slot="props" field="entityType" label="TYPE">
-              <StringValue :string-value="props.row.type"/>
-            </o-table-column>
-
-            <o-table-column v-slot="props" field="website" label="WEB SITE">
-              <StringValue :string-value="props.row.website"/>
-            </o-table-column>
-
-          </o-table>
-
-        </template>
-
-      </DashboardCardV2>
+      <ProfileContent/>
 
     </template>
 
@@ -181,13 +55,7 @@ import {ProfileConnectionStatus, ProfileController} from "@/utils/profile/Profil
 import ButtonView from "@/elements/ButtonView.vue";
 import TextFieldView from "@/elements/TextFieldView.vue";
 import ReCaptcha from "@/components/recaptcha/ReCaptcha.vue";
-import {Portal} from "@/utils/profile/Portal.ts";
-import DashboardCardV2 from "@/components/DashboardCardV2.vue";
-import {ButtonSize} from "@/dialogs/core/DialogUtils.ts";
-import Property from "@/components/Property.vue";
-import StringValue from "@/components/values/StringValue.vue";
-import {ORUGA_MOBILE_BREAKPOINT} from "@/BreakPoints.ts";
-import AccountLink from "@/components/values/link/AccountLink.vue";
+import ProfileContent from "@/components/profile/ProfileContent.vue";
 
 const profileController = ProfileController.inject()
 const connectionStatus = profileController.connectionStatus
@@ -207,45 +75,8 @@ const connectEnabled = computed(() =>
     && (recaptchaKey.value === null || recaptchaToken.value !== null)
 )
 
-const email = computed(() => profileController.user.value?.email ?? null)
-const firstName = computed(() => profileController.user.value?.profile?.firstName ?? null)
-const lastName = computed(() => profileController.user.value?.profile?.lastName ?? null)
-
-const DEFAULT_PAGE_SIZE = 15
-const perPage = ref(DEFAULT_PAGE_SIZE)
-
-const bookmarks = profileController.bookmarks
-const ed25519AccountId = profileController.ed25519AccountId
-const ecdsaAccountId = profileController.ecdsaAccountId
-
 const handleConnect = async () => {
   await profileController.connect(emailText.value, passwordText.value, recaptchaToken.value!)
-}
-
-const handleAddBookmark = async () => {
-  const newBookmark: Portal.NewEntityBookmark = {
-    name: "Testnet ECDSA",
-    type: null,
-    description: "My testnet account with ECDSA key",
-    website: null,
-    networkEpoch: "1746726609.785794442",
-    entityType: "account",
-    publicKey: "0x302d300706052b8104000a03220003d236ba45caea9dd8053b6b0db1953564a4d06c9fb7dbf93bec499e6362b5b45f"
-  }
-  try {
-    const bookmark = await profileController.portalClient?.writeBookmark("testnet", "0.0.1584", newBookmark)
-    console.log("bookmark = " + JSON.stringify(bookmark, null, "  "))
-  } catch(error) {
-    console.log("error=" + error)
-  }
-}
-
-const handleRemoveBookmark = async () => {
-  try {
-    await profileController.portalClient?.clearBookmark("testnet", "0.0.1584")
-  } catch(error) {
-    console.log("error=" + error)
-  }
 }
 
 </script>
