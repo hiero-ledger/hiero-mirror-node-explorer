82aad4aa17cdb5af69f72e2d943226245bf72b33
Simon Viénot <simon.vienot@icloud.com>
2025-05-23 16:58:55 +0200

    Make BookmarkLabel editable.
    Signed-off-by: Simon Viénot <simon.vienot@icloud.com>

--- src/components/values/BookmarkLabel.vue
+++ src/components/values/BookmarkLabel.vue
@@ -5,17 +5,29 @@
 <!-- --------------------------------------------------------------------------------------------------------------- -->
 
 <template>
-  <EntityLabel :label="name" :url="website" :compact="props.compact">
-    <template #icon>
-      <Bookmark :size="12" :class="{'low-contrast':props.compact}"/>
-    </template>
+  <div class="hover-container">
+    <EntityLabel
+        :label="name"
+        :url="website"
+        :compact="props.compact"
+    >
+      <template #icon>
+        <Bookmark :size="12" :class="{'low-contrast':props.compact}"/>
+      </template>
 
-    <template #tooltip>
-      <p>{{ `Bookmark for ID ${entityId}` + (type ? ` [${type}]` : '') }}</p>
-      <p>{{ description }}</p>
-      <p>{{ website }}</p>
-    </template>
-  </EntityLabel>
+      <template #tooltip>
+        <p>{{ `Bookmark for ID ${entityId}` + (type ? ` [${type}]` : '') }}</p>
+        <p>{{ description }}</p>
+        <p>{{ website }}</p>
+      </template>
+    </EntityLabel>
+    <Pencil
+        v-if="!props.compact && props.editable"
+        :size="16"
+        @click="onEdit()"
+        class="shy-edit-icon"
+    />
+  </div>
 </template>
 
 <!-- --------------------------------------------------------------------------------------------------------------- -->
@@ -25,7 +37,7 @@
 <script setup lang="ts">
 
 import {computed, PropType} from "vue";
-import {Bookmark} from 'lucide-vue-next';
+import {Bookmark, Pencil} from 'lucide-vue-next';
 import EntityLabel from "@/components/values/EntityLabel.vue";
 import {Portal} from "@/utils/profile/Portal.ts";
 
@@ -38,7 +50,17 @@ const props = defineProps({
     type: Boolean,
     default: false
   },
+  editable: {
+    type: Boolean,
+    default: true
+  }
 })
+
+const emit = defineEmits(['edit'])
+
+const onEdit = () => {
+  emit('edit')
+}
 
 const name = computed(() => props.entityBookmark?.name ?? null)
 const entityId = computed(() => props.entityBookmark?.entityId ?? null)
@@ -53,6 +75,25 @@ const type = computed(() => props.entityBookmark?.type ?? null)
 <!-- --------------------------------------------------------------------------------------------------------------- -->
 
 <style scoped>
+
+.hover-container {
+  align-items: center;
+  display: flex;
+  gap: 4px
+}
+
+.shy-edit-icon {
+  color: var(--network-text-accent-color);
+  cursor: pointer;
+  opacity: 0;
+  transition: opacity 0.3s ease;
+  visibility: hidden;
+}
+
+.hover-container:hover .shy-edit-icon {
+  opacity: 1;
+  visibility: visible;
+}
 
 .low-contrast {
   color: var(--text-secondary);
