78f58da273999c7906372f75aeba6360ba38d507
Eric Le Ponner <eric.leponner@icloud.com>
2025-05-27 17:19:11 +0200

    AddressDetails view now uses <script setup> syntax.
    Signed-off-by: Eric Le Ponner <eric.leponner@icloud.com>

--- src/pages/AddressDetails.vue
+++ src/pages/AddressDetails.vue
@@ -12,67 +12,57 @@
 <!--                                                      SCRIPT                                                     -->
 <!-- --------------------------------------------------------------------------------------------------------------- -->
 
-<script lang="ts">
+<script setup lang="ts">
 
-import {defineComponent, onMounted} from 'vue';
+import {onMounted} from 'vue';
 import router, {routeManager} from "@/router";
 import {PathParam} from "@/utils/PathParam";
 import {RouteLocationRaw} from "vue-router";
 import {ContractByAddressCache} from "@/utils/cache/ContractByAddressCache";
 import {AccountByAddressCache} from "@/utils/cache/AccountByAddressCache";
 
-export default defineComponent({
+const props = defineProps({
+  accountAddress: String,
+  network: String
+})
 
-  name: 'AddressDetails',
+const getContractId = async (evmAddress: string): Promise<string | null> => {
+  const contract = await ContractByAddressCache.instance.lookup(evmAddress)
+  return Promise.resolve(contract?.contract_id ?? null)
+}
 
-  components: {},
+const getAccountId = async (evmAddress: string): Promise<string | null> => {
+  const account = await AccountByAddressCache.instance.lookup(evmAddress)
+  return Promise.resolve(account?.account ?? null)
+}
 
-  props: {
-    accountAddress: String,
-    network: String
-  },
+const selectRoute = async () => {
+  let result: RouteLocationRaw
 
-  setup(props) {
+  const evmAddress = PathParam.parseEvmAddress(props.accountAddress)
+  if (evmAddress !== null) {
 
-    const getContractId = async (evmAddress: string): Promise<string | null> => {
-      const contract = await ContractByAddressCache.instance.lookup(evmAddress)
-      return Promise.resolve(contract?.contract_id ?? null)
-    }
-
-    const getAccountId = async (evmAddress: string): Promise<string | null> => {
-      const account = await AccountByAddressCache.instance.lookup(evmAddress)
-      return Promise.resolve(account?.account ?? null)
-    }
-
-    const selectRoute = async () => {
-      let result: RouteLocationRaw
-
-      const evmAddress = PathParam.parseEvmAddress(props.accountAddress)
-      if (evmAddress !== null) {
-
-        const contractId = await getContractId(evmAddress)
-        if (contractId !== null) {
-          result = routeManager.makeRouteToContract(contractId)
-        } else {
-          const accountId = await getAccountId(evmAddress)
-          if (accountId !== null) {
-            result = routeManager.makeRouteToAccount(accountId)
-          } else {
-            result = routeManager.makeRouteToAccount(evmAddress)
-          }
-        }
-
+    const contractId = await getContractId(evmAddress)
+    if (contractId !== null) {
+      result = routeManager.makeRouteToContract(contractId)
+    } else {
+      const accountId = await getAccountId(evmAddress)
+      if (accountId !== null) {
+        result = routeManager.makeRouteToAccount(accountId)
       } else {
-        result = routeManager.makeRouteToPageNotFound()
+        result = routeManager.makeRouteToAccount(evmAddress)
       }
-
-      return Promise.resolve(result)
     }
 
-    onMounted(() => {
-      selectRoute().then((route: RouteLocationRaw) => router.replace(route))
-    })
+  } else {
+    result = routeManager.makeRouteToPageNotFound()
   }
+
+  return Promise.resolve(result)
+}
+
+onMounted(() => {
+  selectRoute().then((route: RouteLocationRaw) => router.replace(route))
 })
 
 </script>
