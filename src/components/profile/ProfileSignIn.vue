fb765b3514da2044b5d32181c5ee4aefb803bbca
Simon Viénot <simon.vienot@icloud.com>
2025-05-28 15:08:06 +0200

    Tweak ProfileSignIn.
    Signed-off-by: Simon Viénot <simon.vienot@icloud.com>

--- src/components/profile/ProfileSignIn.vue
+++ src/components/profile/ProfileSignIn.vue
@@ -6,8 +6,8 @@
 
 <template>
   <div class="sign-in-root">
-    <div class="sign-in-line1">Connection to Hedera Portal</div>
-    <div class="sign-in-line2">If you have an account on Hedera Portal, connect with it and benefit form extra features like entity bookmarks.</div>
+    <div class="sign-in-line1">Connection to HashScan Backend</div>
+    <div class="sign-in-line2">If you have an account on Hedera Portal, you may use it to connect and benefit from additional features like entity bookmarks.</div>
     <div class="sign-in-form">
       <TextFieldView v-model="emailText" placeholder="e-Mail"/>
       <TextFieldView v-model="passwordText" placeholder="Password" type="password"/>
