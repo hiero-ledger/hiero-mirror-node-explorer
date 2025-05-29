2231e9e175fbe9794a8465068c077a4d1b86c602
Simon Viénot <simon.vienot@icloud.com>
2025-05-30 00:23:07 +0200

    Add definitions for BlockscoutTokenBalance.
    Signed-off-by: Simon Viénot <simon.vienot@icloud.com>

--- src/schemas/BlockScoutSchemas.ts
+++ src/schemas/BlockScoutSchemas.ts
@@ -1,27 +1,45 @@
 // SPDX-License-Identifier: Apache-2.0
 
-// ---------------------------------------------------------------------------------------------------------------------
-//                                                      Account
-// ---------------------------------------------------------------------------------------------------------------------
-
 //
 // https://eth.blockscout.com/api-docs
 //
 
 export interface BlockscoutTokenInfo {
-    circulating_market_cap: string|null
-    icon_url: string|null
-    name: string|null
-    decimals: string|null
-    symbol: string|null
+    circulating_market_cap: string | null
+    icon_url: string | null
+    name: string | null
+    decimals: string | null
+    symbol: string | null
     address: string
     type: string
-    holders: string|null
-    exchange_rate: string|null
+    holders: string | null
+    exchange_rate: string | null
     total_supply: string
 }
 
 export interface BlockscoutTokenResponse {
     items: BlockscoutTokenInfo[]
     next_page_params: object
+}
+
+export interface BlockscoutTokenBalance {
+    token_instance?: BlockScoutNFTInstance | null
+    value: string
+    token_id: string | null
+    token: BlockscoutTokenInfo
+}
+
+export interface BlockScoutNFTInstance {
+    is_unique: boolean | null
+    id: string
+    holder_address_hash: string
+    image_url: string | null
+    animation_url: string
+    external_app_url: string | null
+    media_type: string | null
+    media_url: string | null
+    metadata: any | null
+    owner: any | null
+    thumbnails: any | null
+    token: BlockscoutTokenInfo
 }
