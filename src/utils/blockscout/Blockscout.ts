1cbcbd27ebfabdf4384c9b0ddf7fd13028d7ce1e
Eric Le Ponner <eric.leponner@icloud.com>
2025-05-30 11:38:30 +0200

    Added Blockscout.TableController.
    Signed-off-by: Eric Le Ponner <eric.leponner@icloud.com>

--- src/utils/blockscout/Blockscout.ts
+++ src/utils/blockscout/Blockscout.ts
@@ -1,5 +1,9 @@
 // SPDX-License-Identifier: Apache-2.0
 
+import {computed, ComputedRef, ref, Ref, watch} from "vue";
+import {AppStorage} from "@/AppStorage.ts";
+import axios from "axios";
+
 export namespace Blockscout {
 
     //
@@ -26,4 +30,182 @@ export namespace Blockscout {
 
     export type TokenInfoResponse = Response<TokenInfo>
     export type NextPageParams = Record<string, unknown>
+}
+
+
+//
+// Blockscout.TableController
+//
+
+export namespace Blockscout {
+
+    export class TableController<R> {
+
+        private readonly buffer: RowBuffer<R>
+
+        //
+        // Public
+        //
+
+        public pageSize: Ref<number>
+
+        public readonly currentPage: Ref<number> = computed(() => this.buffer.currentPage.value)
+
+        public readonly rows = computed<R[]>(() => this.buffer.currentRows.value)
+
+        public readonly totalRowCount: ComputedRef<number> = computed(() => {
+            let result: number
+            if (this.buffer.drained.value) {
+                result = this.buffer.allRowCount.value
+            } else {
+                const k = Math.ceil((this.buffer.allRowCount.value + 1) / this.presumedRowCount.value)
+                result = k * this.presumedRowCount.value
+            }
+            return result
+        })
+
+        public readonly loading = computed(() => this.buffer.loading.value)
+
+        public readonly paginated: ComputedRef<boolean> = computed(
+            () => this.totalRowCount.value >= this.pageSize.value)
+
+
+        public readonly onPageChange = (page: number): void => {
+            this.buffer.move(page, this.pageSize.value).finally()
+        }
+
+        public mount(): void {
+            this.onPageChange(1)
+        }
+
+        public unmount(): void {
+            this.buffer.reset()
+        }
+
+        //
+        // Public (to be subclassed)
+        //
+
+        public async load(nextPageParams: NextPageParams|null, blockscoutURL: string): Promise<Response<R>|null> {
+            throw new Error("To be subclassed: nextPageParams=" + nextPageParams + ", blockscoutURL=" + blockscoutURL)
+        }
+
+        //
+        // Protected
+        //
+
+        protected constructor(private readonly defaultPageSize: number,
+                              private readonly blockscoutURL: Ref<string|null>,
+                              pageSizeStorageKey: string) {
+            this.pageSize = ref(AppStorage.getTablePageSize(pageSizeStorageKey) ?? defaultPageSize)
+            this.buffer = new RowBuffer<R>(this)
+            watch(this.pageSize, () => AppStorage.setTablePageSize(pageSizeStorageKey, this.pageSize.value))
+        }
+
+        //
+        // Private
+        //
+
+        public async loadNext(nextPageParams: NextPageParams|null): Promise<Response<R>|null> {
+            let result: Response<R>|null
+            if (this.blockscoutURL.value !== null) {
+                result = await this.load(nextPageParams, this.blockscoutURL.value)
+            } else {
+                result = { items: [], next_page_params: null}
+            }
+            return Promise.resolve(result)
+        }
+
+        private readonly presumedRowCount = computed(() => this.pageSize.value * 10)
+    }
+
+    class RowBuffer<R> {
+
+        public readonly loading = ref(false)
+        private readonly allRows = ref<R[]>([]) as Ref<R[]>
+        private readonly startIndex = ref<number>(-1)
+        private readonly pageSize = ref<number>(10)
+        private readonly nextPageParams = ref<NextPageParams|null>(null)
+        private readonly loadCount = ref(0)
+
+        constructor(private readonly controller: TableController<R>) {}
+
+        readonly currentRows = computed<R[]>(() => {
+            let result: R[]
+            if (this.startIndex.value !== -1) {
+                result = this.allRows.value.slice(this.startIndex.value, this.endIndex.value)
+            } else {
+                result = []
+            }
+            return result
+        })
+
+        readonly currentPage = computed<number>(() => {
+            let result: number
+            if (this.startIndex.value !== -1) {
+                result = Math.floor(this.startIndex.value / this.pageSize.value) + 1
+            } else {
+                result = -1
+            }
+            return result
+        })
+
+        readonly allRowCount = computed(
+            () => this.allRows.value.length)
+
+        readonly drained = computed(
+            () => this.loadCount.value >= 1 && this.nextPageParams.value === null)
+
+        async move(pageNo: number, pageSize: number): Promise<void> {
+            const newStartIndex = (pageNo - 1) * pageSize
+            const newEndIndex = newStartIndex + pageSize
+            while (newEndIndex > this.allRows.value.length && !this.drained.value) {
+                await this.loadNext()
+            }
+            if (newStartIndex < this.allRows.value.length) {
+                this.startIndex.value = newStartIndex
+                this.pageSize.value = pageSize
+            }
+            // else leaves this.startIndex and this.pageSize unchanged
+        }
+
+        reset(): void {
+            this.allRows.value = []
+            this.startIndex.value = -1
+            this.pageSize.value = 10
+            this.nextPageParams.value = null
+            this.loadCount.value = 0
+        }
+
+        //
+        // Private
+        //
+
+        private readonly endIndex = computed(() => {
+            let result: number
+            if (this.startIndex.value !== -1) {
+                result = Math.min(this.startIndex.value + this.pageSize.value, this.allRows.value.length)
+            } else {
+                result = -1
+            }
+            return result
+        })
+
+        private async loadNext(): Promise<void> {
+            this.loading.value = true
+            this.loadCount.value += 1
+            try {
+                const r = await this.controller.loadNext(this.nextPageParams.value)
+                this.allRows.value = this.allRows.value.concat(r?.items ?? [])
+                this.nextPageParams.value = r?.next_page_params ?? null
+            } catch(reason) {
+                // Leaves this.allRows unchanged
+                this.nextPageParams.value = null
+                console.error(reason)
+            } finally {
+                this.loading.value = false
+            }
+        }
+    }
+
 }
