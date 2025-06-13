d35bd231862ec799c674de57c241cfc9e1aa749e
Eric Le Ponner <eric.leponner@icloud.com>
2025-06-13 15:23:28 +0200

    Added CounterController and CounterView (to be used later).
    Signed-off-by: Eric Le Ponner <eric.leponner@icloud.com>

---
+++ src/charts/core/CounterController.ts
@@ -0,0 +1,168 @@
+// SPDX-License-Identifier: Apache-2.0
+
+import {computed, ref, Ref, watch, WatchStopHandle} from "vue";
+import axios, {AxiosRequestConfig} from "axios";
+import {RouteManager} from "@/utils/RouteManager.ts";
+
+export abstract class CounterController<M> {
+
+    private readonly metric: Ref<M|null> = ref(null)
+    private readonly loading: Ref<boolean> = ref(false)
+    private readonly error: Ref<unknown> = ref(null)
+    private watchHandle: WatchStopHandle|null = null
+
+    //
+    // Public
+    //
+
+    public mount(): void {
+        this.watchHandle = watch(this.routeManager.currentNetworkEntry, this.updateMetric, {immediate: true})
+    }
+
+    public unmount(): void {
+        if (this.watchHandle !== null) {
+            this.watchHandle()
+            this.watchHandle = null
+        }
+    }
+
+    public readonly value = computed(() => {
+        const m = this.metric.value
+        return m !== null ? this.metricToValue(m) : ""
+    })
+
+    //
+    // Protected
+    //
+
+    protected constructor(
+        public readonly title: string,
+        public readonly info: string,
+        protected readonly routeManager: RouteManager) {}
+
+    protected async loadData(): Promise<M|null> {
+        throw "to be subclassed"
+    }
+
+    protected abstract metricToValue(m: M): string
+
+    //
+    // Private
+    //
+
+    private readonly updateMetric = async () => {
+        this.loading.value = true
+        try {
+            this.metric.value = await this.loadData()
+            this.error.value = null
+        } catch(error) {
+            this.metric.value = null
+            this.error.value = error
+        } finally {
+            this.loading.value = false
+        }
+    }
+
+}
+
+
+interface TransactionAggregate {
+    aggregate: {
+        count: number
+    }
+}
+
+interface GraphQLResponse {
+    data?: {
+        transaction_aggregate?: TransactionAggregate
+    }
+    errors?: unknown[]
+}
+
+export class TransactionCounterController extends CounterController<TransactionAggregate> {
+
+    public static readonly ACCOUNT_CREATE = 11
+
+    //
+    // Public
+    //
+
+    public constructor(
+        private readonly transactionType: number,
+        title: string, info: string, routeManager: RouteManager) {
+        super(title, info, routeManager)
+    }
+
+    //
+    // CounterController
+    //
+
+    protected async loadData(): Promise<TransactionAggregate|null> {
+        let result: TransactionAggregate|null
+
+        const url = this.routeManager.hgraphURL.value
+        if (url !== null) {
+            const query = this.makeQuery(this.transactionType)
+            result = await this.runQuery(url, query)
+        } else {
+            result = null
+        }
+
+        return Promise.resolve(result)
+    }
+
+    protected metricToValue(m: TransactionAggregate): string {
+        return m.aggregate.count.toString()
+    }
+
+
+    //
+    // Private
+    //
+
+    private makeConfig(): AxiosRequestConfig {
+        let result: AxiosRequestConfig
+        const hgraphKey = this.routeManager.hgraphKey.value
+        if (hgraphKey !== null) {
+            result = {
+                headers: {
+                    "X-API-KEY": hgraphKey
+                }
+            }
+        } else {
+            result = {}
+        }
+        return result
+    }
+
+    private async runQuery(url: string, query: string): Promise<TransactionAggregate|null> {
+        let result: TransactionAggregate|null
+
+        const config = this.makeConfig()
+        const response = await axios.post<GraphQLResponse>(url, {query}, config)
+        if (response.status === 200 && typeof response.data === "object" && response.data !== null) {
+            if (response.data.data) {
+                result = response.data.data.transaction_aggregate ?? null
+            } else {
+                const errors = response.data.errors ?? []
+                const error = errors.length >= 1 ? errors[0] : null
+                throw error ?? "GraphQL query failed"
+            }
+        } else {
+            throw "HTTP Error " + response.status
+        }
+
+        return Promise.resolve(result)
+    }
+
+    protected makeQuery(transactionType: number): string {
+        return "{" +
+            "  transaction_aggregate(where: {type: {_eq: \"" + transactionType + "\"}}) {" +
+            "    aggregate {" +
+            "      count" +
+            "    }" +
+            "  }" +
+            "}"
+    }
+
+}
