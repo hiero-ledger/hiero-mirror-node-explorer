c4e5633aae95e48bdaed1545f03a8a9ef5d5f85a
Eric Le Ponner <eric.leponner@icloud.com>
2025-06-13 15:23:28 +0200

    Added CounterController and CounterView (to be used later).
    Signed-off-by: Eric Le Ponner <eric.leponner@icloud.com>

---
+++ src/charts/core/CounterView.vue
@@ -0,0 +1,41 @@
+// SPDX-License-Identifier: Apache-2.0
+
+<!-- --------------------------------------------------------------------------------------------------------------- -->
+<!--                                                     TEMPLATE                                                    -->
+<!-- --------------------------------------------------------------------------------------------------------------- -->
+
+<template>
+  <MarketDashboardItem :title="title" :info="info" :value="value"/>
+</template>
+
+<!-- --------------------------------------------------------------------------------------------------------------- -->
+<!--                                                      SCRIPT                                                     -->
+<!-- --------------------------------------------------------------------------------------------------------------- -->
+
+<script setup lang="ts">
+
+import {PropType} from "vue";
+import MarketDashboardItem from "@/components/dashboard/MarketDashboardItem.vue";
+import {CounterController} from "@/charts/core/CounterController.ts";
+
+
+const props = defineProps({
+  controller: {
+    type: Object as PropType<CounterController<unknown>>,
+    required: true
+  }
+})
+
+const title = props.controller.title
+const info = props.controller.info
+const value = props.controller.value
+
+</script>
+
+<!-- --------------------------------------------------------------------------------------------------------------- -->
+<!--                                                       STYLE                                                     -->
+<!-- --------------------------------------------------------------------------------------------------------------- -->
+
+<style scoped>
+
+</style>
