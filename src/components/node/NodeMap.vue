a30dcffeda16b26e915af36216746c7881e5cc29
Eric Le Ponner <eric.leponner@icloud.com>
2025-06-16 16:02:07 +0200

    Nodes page now displays NodeMap (based on experimental WorldMap).
    Signed-off-by: Eric Le Ponner <eric.leponner@icloud.com>

---
+++ src/components/node/NodeMap.vue
@@ -0,0 +1,78 @@
+// SPDX-License-Identifier: Apache-2.0
+
+<!-- --------------------------------------------------------------------------------------------------------------- -->
+<!--                                                     TEMPLATE                                                    -->
+<!-- --------------------------------------------------------------------------------------------------------------- -->
+
+<template>
+  <WorldMap :annotations="annotations"/>
+</template>
+
+<!-- --------------------------------------------------------------------------------------------------------------- -->
+<!--                                                      SCRIPT                                                     -->
+<!-- --------------------------------------------------------------------------------------------------------------- -->
+
+<script setup lang="ts">
+
+import {onMounted, PropType, ref, watch} from "vue";
+import WorldMap from "@/components/node/map/WorldMap.vue";
+import {NetworkNode} from "@/schemas/MirrorNodeSchemas.ts";
+import {MapAnnotation} from "@/components/node/map/MapAnnotation.ts";
+import {makeNodeOwnerDescription} from "@/schemas/MirrorNodeUtils.ts";
+import {GeoLocationCache} from "@/utils/cache/GeoLocationCache.ts";
+
+const props = defineProps({
+  nodes: {
+    type: Object as PropType<Array<NetworkNode>>,
+    required: true
+  },
+  stakeTotal: Number
+})
+
+const updateAnnotations = async () => {
+
+  const geoLocationBook = await GeoLocationCache.instance.lookup()
+
+  const newAnnotations: MapAnnotation[] = []
+  for (const p of geoLocationBook.places) {
+    newAnnotations.push({
+      lat: p.lat,
+      lon: p.lon,
+      title: p.name,
+      subTitle: ""
+    })
+  }
+
+  // const newAnnotations: MapAnnotation[] = []
+  // for (const node of props.nodes) {
+  //   if (node.public_key !== null) {
+  //     const place = GeoLocationCache.instance.lookup(node.public_key)
+  //     const nl = nodeLocationMap.get(node.public_key)
+  //     if (nl) {
+  //       newAnnotations.push({
+  //         lat: nl.lat,
+  //         lon: nl.lon,
+  //         title: makeNodeOwnerDescription(node, true),
+  //         subTitle: ""
+  //       })
+  //     }
+  //   }
+  // }
+
+  annotations.value = newAnnotations
+}
+
+const annotations = ref<MapAnnotation[]>([])
+onMounted(() => {
+  watch(() => props.nodes, updateAnnotations, {immediate: true})
+})
+
+</script>
+
+<!-- --------------------------------------------------------------------------------------------------------------- -->
+<!--                                                       STYLE                                                     -->
+<!-- --------------------------------------------------------------------------------------------------------------- -->
+
+<style scoped>
+
+</style>
