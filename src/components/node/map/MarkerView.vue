f363adaf9b2ebd4ac1a3bd527c8e2671d63427c6
Eric Le Ponner <eric.leponner@icloud.com>
2025-06-18 13:03:43 +0200

    Added labelPlacement support.
    Signed-off-by: Eric Le Ponner <eric.leponner@icloud.com>

--- src/components/node/map/MarkerView.vue
+++ src/components/node/map/MarkerView.vue
@@ -5,9 +5,9 @@
 <!-- --------------------------------------------------------------------------------------------------------------- -->
 
 <template>
-  <div ref="containerRef" class="container" :style="{left: iconX + 'px', top: iconY + 'px' }">
-    <MapPin :size="iconSize"/>
-    <div>{{ props.title }}</div>
+  <div ref="containerRef" class="container" :style="containerStyle">
+    <Circle :size="iconSize"/>
+    <div style="font-size: 12px">{{ props.title }}</div>
   </div>
 </template>
 
@@ -17,9 +17,10 @@
 
 <script setup lang="ts">
 
-import {MapPin} from 'lucide-vue-next';
-import {computed, PropType, useTemplateRef} from "vue";
+import {Circle} from 'lucide-vue-next';
+import {computed, PropType, StyleValue, useTemplateRef} from "vue";
 import {useElementSize} from "@vueuse/core";
+import {LabelPlacement} from "@/utils/cache/GeoLocationCache.ts";
 
 const props = defineProps({
   xy: {
@@ -33,15 +34,44 @@ const props = defineProps({
   subTitle: {
     type: String,
     default: ""
+  },
+  placement: {
+    type: String as PropType<LabelPlacement>,
+    default: "south"
   }
 })
 
 const containerRef = useTemplateRef<HTMLDivElement>("containerRef")
 const containerSize = useElementSize(containerRef)
 
-const iconSize = 24
-const iconX = computed(() => props.xy.x - containerSize.width.value / 2)
-const iconY = computed(() => props.xy.y - iconSize)
+const iconSize = 10
+
+const containerStyle = computed(() => {
+  let result: StyleValue = {}
+  switch(props.placement) {
+    case "north":
+      result["left"]    = (props.xy.x - containerSize.width.value / 2) + "px"
+      result["top"]     = (props.xy.y - containerSize.height.value + iconSize / 2) + "px"
+      result["flex-direction"] = "column-reverse"
+      break
+    case "south":
+      result["left"]    = (props.xy.x - containerSize.width.value / 2) + "px"
+      result["top"]     = (props.xy.y - iconSize / 2) + "px"
+      result["flex-direction"] = "column"
+      break
+    case "west":
+      result["left"]    = (props.xy.x - containerSize.width.value + iconSize / 2) + "px"
+      result["top"]     = (props.xy.y - containerSize.height.value / 2) + "px"
+      result["flex-direction"] = "row-reverse"
+      break
+    case "east":
+      result["left"]    = (props.xy.x - iconSize / 2) + "px"
+      result["top"]     = (props.xy.y - containerSize.height.value / 2) + "px"
+      result["flex-direction"] = "row"
+      break
+  }
+  return result
+})
 
 </script>
 
@@ -53,8 +83,9 @@ const iconY = computed(() => props.xy.y - iconSize)
 
 div.container {
   display: flex;
-  flex-direction: column;
-  align-items: center;
   position: absolute;
+  align-items: center;
+  column-gap: 2px;
 }
+
 </style>
