8a7fcb5e3b55d129acc8a77d161141f067f25cea
Eric Le Ponner <eric.leponner@icloud.com>
2025-06-18 13:03:43 +0200

    Added labelPlacement support.
    Signed-off-by: Eric Le Ponner <eric.leponner@icloud.com>

--- src/components/node/map/MapAnnotation.ts
+++ src/components/node/map/MapAnnotation.ts
@@ -1,8 +1,11 @@
 // SPDX-License-Identifier: Apache-2.0
 
+import {LabelPlacement} from "@/utils/cache/GeoLocationCache.ts";
+
 export interface MapAnnotation {
     lat: number,
     lon: number,
     title: string,
-    subTitle: string
+    subTitle: string,
+    placement?: LabelPlacement,
 }
