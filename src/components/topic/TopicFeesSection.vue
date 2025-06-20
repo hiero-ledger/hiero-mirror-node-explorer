682bf672e6699ce3e4c66b206ffd8479e3a0e9b8
Simon Viénot <simon.vienot@icloud.com>
2025-06-20 18:38:11 +0200

    TopicFeesSection now in charge of displaying 'Fee Exempt Key List' property.
    Signed-off-by: Simon Viénot <simon.vienot@icloud.com>

--- src/components/topic/TopicFeesSection.vue
+++ src/components/topic/TopicFeesSection.vue
@@ -13,6 +13,7 @@
     </template>
 
     <template #content>
+
       <Property id="custom-fee-created-at" full-width>
         <template #name>Created at</template>
         <template #value>
@@ -25,6 +26,21 @@
         <template #value>
           <FixedFeeTable v-if="fixedFees.length" :fees="fixedFees"/>
           <div v-else class="h-is-low-contrast">None</div>
+        </template>
+      </Property>
+
+      <Property v-if="hasExemptList" id="fee-exempt-key-list" full-width>
+        <template #name>Fee Exempt Key List</template>
+        <template #value>
+          <div class="exempt-list">
+            <KeyValue
+                v-for="k in props.exemptKeyList"
+                :key="k.key"
+                :key-bytes="k.key"
+                :key-type="k._type"
+                :show-none="true"
+            />
+          </div>
         </template>
       </Property>
 
@@ -45,16 +61,22 @@ import TimestampValue from "@/components/values/TimestampValue.vue";
 import Property from "@/components/Property.vue";
 import FixedFeeTable from "@/components/token/FixedFeeTable.vue";
 import DashboardCardV2 from "@/components/DashboardCardV2.vue";
-import {ConsensusCustomFees} from "@/schemas/MirrorNodeSchemas.ts";
+import {ConsensusCustomFees, Key} from "@/schemas/MirrorNodeSchemas.ts";
+import KeyValue from "@/components/values/KeyValue.vue";
 
 const props = defineProps({
   fees: {
     type: Object as PropType<ConsensusCustomFees>,
     required: true
+  },
+  exemptKeyList: {
+    type: Array as PropType<Key[]>,
+    default: []
   }
 })
 
 const fixedFees = computed(() => props.fees.fixed_fees)
+const hasExemptList = computed(() => props.exemptKeyList.length > 0)
 
 </script>
 
@@ -62,4 +84,12 @@ const fixedFees = computed(() => props.fees.fixed_fees)
 <!--                                                       STYLE                                                     -->
 <!-- --------------------------------------------------------------------------------------------------------------- -->
 
-<style/>
+<style scoped>
+
+.exempt-list {
+  display: flex;
+  flex-direction: column;
+  gap: 8px
+}
+
+</style>
