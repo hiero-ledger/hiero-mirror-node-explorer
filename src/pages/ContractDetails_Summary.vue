bf4bdb25625fc9a60a3faaba7cd86c2f88f5dc88
Eric Le Ponner <eric.leponner@icloud.com>
2025-06-24 15:36:30 +0200

    Put everything in DashboardCardV2#content in ContractDetails_Summary.
    Signed-off-by: Eric Le Ponner <eric.leponner@icloud.com>

--- src/pages/ContractDetails_Summary.vue
+++ src/pages/ContractDetails_Summary.vue
@@ -48,37 +48,60 @@
               :address="contractAddress"/>
         </template>
       </Property>
-    </template>
-
-    <template #left-content>
-        <Property id="obtainer">
-          <template #name>Obtainer</template>
-          <template #value>
-            <AccountLink :account-id="obtainerId"/>
-          </template>
-        </Property>
-        <Property id="proxyAccount">
-          <template #name>Proxy Account</template>
-          <template #value>
-            <AccountLink :account-id="proxyAccountId"/>
-          </template>
-        </Property>
-    </template>
-
-    <template #right-content>
-
-      <Property id="file">
+      <Property id="obtainer" full-width>
+        <template #name>Obtainer</template>
+        <template #value>
+          <AccountLink :account-id="obtainerId"/>
+        </template>
+      </Property>
+      <Property id="proxyAccount" full-width>
+        <template #name>Proxy Account</template>
+        <template #value>
+          <AccountLink :account-id="proxyAccountId"/>
+        </template>
+      </Property>
+      <Property id="file" full-width>
         <template #name>File</template>
         <template #value>
           <StringValue :string-value="contract?.file_id"/>
         </template>
       </Property>
+
+      <Property v-if="isVerified" id="verificationStatus" full-width>
+        <template v-slot:name>Verification Status</template>
+        <template v-slot:value>
+          <div class="verification-status">
+            {{ isFullMatch ? "Full Match" : "Partial Match" }}
+            <InfoTooltip :label="tooltipText"/>
+            <ButtonView
+                v-if="!isFullMatch"
+                id="verify-button"
+                :size="ButtonSize.small"
+                @action="showVerifyDialog = true"
+            >
+              RE-VERIFY
+            </ButtonView>
+          </div>
+        </template>
+      </Property>
+      <Property v-if="isVerified" id="contractName" full-width>
+        <template v-slot:name>Contract Name</template>
+        <template v-slot:value>
+          <StringValue :string-value="contractName ?? undefined"/>
+        </template>
+      </Property>
+
     </template>
   </DashboardCardV2>
 
   <ContractERCSection :erc-analyzer="ercAnalyzer"/>
 
   <MirrorLink :network="props.network" entityUrl="contracts" :loc="contractId"/>
+
+  <ContractVerificationDialog
+      v-model:show-dialog="showVerifyDialog"
+      :contract-id="contractId"
+      v-on:verify-did-complete="verifyDidComplete"/>
 
 </template>
 
@@ -88,7 +111,7 @@
 
 <script setup lang="ts">
 
-import {computed, onBeforeUnmount, onMounted} from "vue";
+import {computed, onBeforeUnmount, onMounted, ref} from "vue";
 import AccountLink from "@/components/values/link/AccountLink.vue";
 import DashboardCardV2 from "@/components/DashboardCardV2.vue";
 import DomainLabel from "@/components/values/DomainLabel.vue";
@@ -106,6 +129,10 @@ import {routeManager} from "@/utils/RouteManager.ts";
 import {ERCAnalyzer} from "@/utils/analyzer/ERCAnalyzer.ts";
 import ContractERCSection from "@/components/contract/ContractERCSection.vue";
 import MirrorLink from "@/components/MirrorLink.vue";
+import InfoTooltip from "@/components/InfoTooltip.vue";
+import ButtonView from "@/elements/ButtonView.vue";
+import {ButtonSize} from "@/dialogs/core/DialogUtils.ts";
+import ContractVerificationDialog from "@/dialogs/verification/ContractVerificationDialog.vue";
 
 const props = defineProps({
   contractId: String,
@@ -136,6 +163,11 @@ onMounted(() => contractAnalyzer.mount())
 onBeforeUnmount(() => contractAnalyzer.unmount())
 const contractName = contractAnalyzer.contractName
 const isVerified = contractAnalyzer.isVerified
+const isFullMatch = contractAnalyzer.fullMatch
+const FULL_MATCH_TOOLTIP = `A Full Match indicates that the bytecode of the deployed contract is byte-by-byte the same as the compilation output of the given source code files with the settings defined in the metadata file. This means the contents of the source code files and the compilation settings are exactly the same as when the contract author compiled and deployed the contract.`
+const PARTIAL_MATCH_TOOLTIP = `A Partial Match indicates that the bytecode of the deployed contract is the same as the compilation output of the given source code files except for the metadata hash. This means the deployed contract and the given source code + metadata function in the same way but there are differences in source code comments, variable names, or other metadata fields such as source paths.`
+const tooltipText = computed(() => isFullMatch.value ? FULL_MATCH_TOOLTIP : PARTIAL_MATCH_TOOLTIP)
+
 
 //
 // ERCAnalyzer
@@ -178,6 +210,16 @@ const accountChecksum = computed(() =>
         contractId.value,
         routeManager.currentNetwork.value
     ) : null)
+
+
+//
+// Verify dialog
+//
+const showVerifyDialog = ref(false)
+const verifyDidComplete = () => {
+  contractAnalyzer.verifyDidComplete()
+}
+
 
 </script>
 
