567ffe60379431ef07afcf1c1cb83751b2c0cf6b
Eric Le Ponner <eric.leponner@icloud.com>
2025-06-25 16:12:02 +0200

    Re-worked NodeMap.
    Signed-off-by: Eric Le Ponner <eric.leponner@icloud.com>

---
+++ src/components/node/map/Marker.vue
@@ -0,0 +1,51 @@
+// SPDX-License-Identifier: Apache-2.0
+
+<!-- --------------------------------------------------------------------------------------------------------------- -->
+<!--                                                     TEMPLATE                                                    -->
+<!-- --------------------------------------------------------------------------------------------------------------- -->
+
+<template>
+  <button :style="itemStyle" @click="onClick"/>
+</template>
+
+<!-- --------------------------------------------------------------------------------------------------------------- -->
+<!--                                                      SCRIPT                                                     -->
+<!-- --------------------------------------------------------------------------------------------------------------- -->
+
+<script setup lang="ts">
+
+import {computed} from "vue";
+
+const emits = defineEmits(["action"])
+
+const itemSize = 14
+const itemStyle = computed(() => {
+  return {
+    left: -itemSize / 2 + "px",
+    top: -itemSize / 2 + "px",
+    width: itemSize + "px",
+    height: itemSize + "px",
+    "border-radius": itemSize / 2 + "px"
+  }
+})
+
+const onClick = () => emits("action")
+
+</script>
+
+<!-- --------------------------------------------------------------------------------------------------------------- -->
+<!--                                                       STYLE                                                     -->
+<!-- --------------------------------------------------------------------------------------------------------------- -->
+
+<style scoped>
+
+button {
+  position: absolute;
+  background-color: var(--network-text-accent-color);
+  border: none;
+  text-decoration: none;
+  display: inline-block;
+  cursor: pointer;
+}
+
+</style>
