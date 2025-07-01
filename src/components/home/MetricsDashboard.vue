aedbc51ab7e0b5e26d4e531851f349a164e6a5cb
Simon Viénot <simon.vienot@icloud.com>
2025-07-01 15:20:17 +0200

    Add components for metrics dashboard.
    Signed-off-by: Simon Viénot <simon.vienot@icloud.com>

---
+++ src/components/home/MetricsDashboard.vue
@@ -0,0 +1,91 @@
+// SPDX-License-Identifier: Apache-2.0
+
+<!-- --------------------------------------------------------------------------------------------------------------- -->
+<!--                                                     TEMPLATE                                                    -->
+<!-- --------------------------------------------------------------------------------------------------------------- -->
+
+<template>
+
+  <div class="dashboard-root">
+    <div class="dashboard-content">
+      <CounterView :controller="accountCreateCounterController">
+        <User :size="32"/>
+      </CounterView>
+
+      <div class="mini-line-separator"/>
+
+      <CounterView :controller="contractCreateCounterController">
+        <FileScan :size="32"/>
+      </CounterView>
+
+      <div v-if="isLargeScreen || !isSmallScreen" class="mini-line-separator"/>
+
+      <CounterView :controller="tokenCreateCounterController">
+        <Hexagon :size="32"/>
+      </CounterView>
+
+      <div class="mini-line-separator"/>
+
+      <CounterView :controller="accumulatedTransactionCountController">
+        <MoveHorizontal :size="32"/>
+      </CounterView>
+    </div>
+  </div>
+
+</template>
+
+<!-- --------------------------------------------------------------------------------------------------------------- -->
+<!--                                                      SCRIPT                                                     -->
+<!-- --------------------------------------------------------------------------------------------------------------- -->
+
+<script setup lang="ts">
+
+import {inject, onBeforeUnmount, onMounted, ref} from 'vue';
+import {FileScan, Hexagon, MoveHorizontal, User} from 'lucide-vue-next';
+import CounterView from "@/charts/core/CounterView.vue";
+import {
+  AccumulatedTransactionCounterController,
+  TransactionCounterController
+} from "@/charts/core/CounterController.ts";
+import {routeManager} from "@/utils/RouteManager.ts";
+
+const isSmallScreen = inject('isSmallScreen', ref(true))
+const isLargeScreen = inject('isLargeScreen', ref(true))
+
+const accountCreateCounterController = new TransactionCounterController(
+    TransactionCounterController.ACCOUNT_CREATE, "Account Created", "", routeManager)
+onMounted(() => accountCreateCounterController.mount())
+onBeforeUnmount(() => accountCreateCounterController.unmount())
+
+const contractCreateCounterController = new TransactionCounterController(
+    TransactionCounterController.CONTRACT_CREATE, "Contract Created", "", routeManager)
+onMounted(() => contractCreateCounterController.mount())
+onBeforeUnmount(() => contractCreateCounterController.unmount())
+
+const tokenCreateCounterController = new TransactionCounterController(
+    TransactionCounterController.TOKEN_CREATE, "HTS Token Created", "", routeManager)
+onMounted(() => tokenCreateCounterController.mount())
+onBeforeUnmount(() => tokenCreateCounterController.unmount())
+
+const accumulatedTransactionCountController = new AccumulatedTransactionCounterController(
+    "Total Nb of Transactions", "", routeManager)
+onMounted(() => accumulatedTransactionCountController.mount())
+onBeforeUnmount(() => accumulatedTransactionCountController.unmount())
+
+</script>
+
+<!-- --------------------------------------------------------------------------------------------------------------- -->
+<!--                                                       STYLE                                                     -->
+<!-- --------------------------------------------------------------------------------------------------------------- -->
+
+<style scoped>
+
+div.dashboard-root {
+  background-color: var(--background-tertiary);
+  border: 1px solid var(--table-border);
+  border-radius: 16px;
+  display: flex;
+  justify-content: center;
+}
+
+</style>
