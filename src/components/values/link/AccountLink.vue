3debfc402dba4c4af9fece715e80fddae1d423ad
Eric Le Ponner <eric.leponner@icloud.com>
2025-07-02 20:21:04 +0200

    AccountLink now uses <script setup> syntax.
    Signed-off-by: Eric Le Ponner <eric.leponner@icloud.com>

--- src/components/values/link/AccountLink.vue
+++ src/components/values/link/AccountLink.vue
@@ -26,9 +26,9 @@
 <!--                                                      SCRIPT                                                     -->
 <!-- --------------------------------------------------------------------------------------------------------------- -->
 
-<script lang="ts">
+<script setup lang="ts">
 
-import {computed, defineComponent, onBeforeUnmount, onMounted, PropType, ref, watch} from "vue";
+import {computed, onBeforeUnmount, onMounted, PropType, ref, watch} from "vue";
 import {NetworkCache} from "@/utils/cache/NetworkCache";
 import {ContractByIdCache} from "@/utils/cache/ContractByIdCache";
 import {RouteLocationRaw} from "vue-router";
@@ -37,75 +37,65 @@ import AccountIOL from "@/components/values/link/AccountIOL.vue";
 import {makeOperatorDescription} from "@/schemas/MirrorNodeUtils.ts";
 import {routeManager} from "@/utils/RouteManager.ts";
 
-export default defineComponent({
-  name: "AccountLink",
-  components: {EntityLink, AccountIOL},
-
-  props: {
-    accountId: {
-      type: String as PropType<string | null>,
-      default: null
-    },
-    showExtra: {
-      type: Boolean,
-      default: false
-    },
-    noAnchor: {
-      type: Boolean,
-      default: false
-    },
-    nullLabel: {
-      type: String,
-      default: null
-    },
+const props = defineProps({
+  accountId: {
+    type: String as PropType<string | null>,
+    default: null
   },
+  showExtra: {
+    type: Boolean,
+    default: false
+  },
+  noAnchor: {
+    type: Boolean,
+    default: false
+  },
+  nullLabel: {
+    type: String,
+    default: null
+  },
+})
 
-  setup(props) {
+const networkLookup = NetworkCache.instance.makeLookup()
+onMounted(() => networkLookup.mount())
+onBeforeUnmount(() => networkLookup.unmount())
 
-    const networkLookup = NetworkCache.instance.makeLookup()
-    onMounted(() => networkLookup.mount())
-    onBeforeUnmount(() => networkLookup.unmount())
+const accountRoute = ref<RouteLocationRaw | null>(null)
 
-    const accountRoute = ref<RouteLocationRaw | null>(null)
-
-    const selectRoute = async (accountId: string) => {
-      let result: RouteLocationRaw | null
-      try {
-        if (await ContractByIdCache.instance.lookup(accountId) !== null) {
-          result = routeManager.makeRouteToContract(accountId)
-        } else {
-          result = routeManager.makeRouteToAccount(accountId)
-        }
-      } catch {
-        result = null
-      }
-      return Promise.resolve(result)
+const selectRoute = async (accountId: string) => {
+  let result: RouteLocationRaw | null
+  try {
+    if (await ContractByIdCache.instance.lookup(accountId) !== null) {
+      result = routeManager.makeRouteToContract(accountId)
+    } else {
+      result = routeManager.makeRouteToAccount(accountId)
     }
-
-    onMounted(() => {
-      if (props.accountId && !props.noAnchor) {
-        selectRoute(props.accountId).then((route) => accountRoute.value = route)
-      } else {
-        accountRoute.value = null
-      }
-    })
-    watch(() => props.accountId, (newValue) => {
-      if (newValue) {
-        selectRoute(newValue).then((route) => accountRoute.value = route)
-      } else {
-        accountRoute.value = null
-      }
-    })
-    const nodes = computed(() => networkLookup.entity.value ?? [])
-
-    const extra = computed(() => {
-      const result = props.accountId ? makeOperatorDescription(props.accountId, nodes.value) : null
-      return result ?? ""
-    })
-
-    return {accountRoute, extra}
+  } catch {
+    result = null
   }
-});
+  return Promise.resolve(result)
+}
+
+onMounted(() => {
+  if (props.accountId && !props.noAnchor) {
+    selectRoute(props.accountId).then((route) => accountRoute.value = route)
+  } else {
+    accountRoute.value = null
+  }
+})
+watch(() => props.accountId, (newValue) => {
+  if (newValue) {
+    selectRoute(newValue).then((route) => accountRoute.value = route)
+  } else {
+    accountRoute.value = null
+  }
+})
+const nodes = computed(() => networkLookup.entity.value ?? [])
+
+const extra = computed(() => {
+  const result = props.accountId ? makeOperatorDescription(props.accountId, nodes.value) : null
+  return result ?? ""
+})
 
 </script>
 
