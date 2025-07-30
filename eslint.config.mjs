e98f2b3e9b655e7b205dfa00d810d244a975c86b
Eric Le Ponner <91124272+ericleponner@users.noreply.github.com>
2025-07-30 11:40:52 +0200

    chore: separate config for src/ and tests/ in eslint.config.mjs (#2173)
    Signed-off-by: Eric Le Ponner <eric.leponner@icloud.com>

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -8,13 +8,13 @@ import globals from 'globals'
 export default tseslint.config(
 //    { ignores: ['*.d.ts', '**/coverage', '**/dist'] },
     {
+        files: ['src/**/*.{ts,vue}'],
         extends: [
             eslint.configs.recommended,
             ...tseslint.configs.recommended,
             ...eslintPluginVue.configs['flat/essential'],
             pluginCypress.configs.recommended
         ],
-        files: ['**/*.{ts,vue}'],
         languageOptions: {
             ecmaVersion: 'latest',
             sourceType: 'module',
@@ -25,13 +25,35 @@ export default tseslint.config(
         },
         rules: {
 
-
             "@typescript-eslint/no-explicit-any": "off",
 
             "vue/no-reserved-component-names": "off",
             "vue/multi-word-component-names": "off",
             "vue/no-mutating-props": "off",
 
+            "complexity": ["error", 8],
+            "max-lines-per-function": ["error", 50]
+        }
+    },
+    {
+        files: ['tests/**/*.ts'],
+        extends: [
+            eslint.configs.recommended,
+            ...tseslint.configs.recommended,
+            ...eslintPluginVue.configs['flat/essential'],
+            pluginCypress.configs.recommended
+        ],
+        languageOptions: {
+            ecmaVersion: 'latest',
+            sourceType: 'module',
+            globals: globals.browser,
+            parserOptions: {
+                parser: tseslint.parser,
+            },
+        },
+        rules: {
+            "max-lines-per-function": "off",
+            "@typescript-eslint/no-explicit-any": "off",
         }
     }
 )
