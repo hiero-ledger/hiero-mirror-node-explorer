7937bbe151cf0d9c8758a9fc198097729042dea5
Eric Le Ponner <eric.leponner@icloud.com>
2025-08-01 18:19:56 +0200

    Added pg module and .env.example.
    Signed-off-by: Eric Le Ponner <eric.leponner@icloud.com>

---
+++ _backend/src/pg/pg.module.ts
@@ -0,0 +1,36 @@
+import { Module } from '@nestjs/common';
+import {ConfigModule, ConfigService} from '@nestjs/config';
+import pg from "pg";
+import {PG_OPTIONS, PG_POOL} from "./pg.constants";
+
+@Module({
+    imports: [ConfigModule],
+    providers: [
+        {
+            provide: PG_OPTIONS,
+            inject: [ConfigService],
+            useFactory: (config: ConfigService): pg.PoolConfig => {
+                return {
+                    host: config.get<string>('PG_HOST', 'localhost'),
+                    port:  config.get<number>('PG_PORT', 5432),
+                    user:  config.get<string>('PG_USER',  "postgres"),
+                    password:  config.get<string>('PG_PASSWORD'),
+                    database:  config.get<string>('PG_DATABASE'),
+                    // Setup from Hedera Portal
+                    max: 75,
+                    min: 1,
+                    maxUses: 7500,
+                    connectionTimeoutMillis: 2000,
+                    idleTimeoutMillis: 10_000,
+                }
+            },
+        },
+        {
+            provide: PG_POOL,
+            inject: [PG_OPTIONS],
+            useFactory: (options: pg.PoolConfig) => new pg.Pool(options),
+        }
+    ],
+    exports: ['PG_POOL'],
+})
+export class PgModule {}
