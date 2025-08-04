6fa2faea34a20b41f98ac4fe2c766e68cd312ef2
Eric Le Ponner <eric.leponner@icloud.com>
2025-08-04 18:12:46 +0200

    Added user table.
    Signed-off-by: Eric Le Ponner <eric.leponner@icloud.com>

--- _backend/migrations/01_setup.sql
+++ _backend/migrations/01_setup.sql
@@ -4,3 +4,25 @@ CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 -- text collation that sorts text case-insensitively, useful for `UNIQUE` indexes
 create collation case_insensitive (provider = icu, locale = 'und-u-ks-level2', deterministic = false);
 
+-- while `created_at` can just be `default now()`, setting `updated_at` on update requires a trigger
+create or replace function set_updated_at()
+    returns trigger as
+$$
+begin
+    NEW.updated_at = now();
+    return NEW;
+end;
+$$ language plpgsql;
+
+create or replace function trigger_updated_at(tablename regclass)
+    returns void as
+$$
+begin
+    execute format('CREATE TRIGGER set_updated_at
+        BEFORE UPDATE
+        ON %s
+        FOR EACH ROW
+        WHEN (OLD is distinct from NEW)
+    EXECUTE FUNCTION set_updated_at();', tablename);
+end;
+$$ language plpgsql;
