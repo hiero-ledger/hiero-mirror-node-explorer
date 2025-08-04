39cb1141570bec9a15bdffa0a9e4e784fc9db961
Eric Le Ponner <eric.leponner@icloud.com>
2025-08-04 10:02:23 +0200

    AppService.getHello() now returns postgres version.
    Signed-off-by: Eric Le Ponner <eric.leponner@icloud.com>

--- _backend/src/app/app.service.ts
+++ _backend/src/app/app.service.ts
@@ -1,8 +1,17 @@
-import { Injectable } from '@nestjs/common';
+import {Inject, Injectable} from '@nestjs/common';
+import {PG_POOL} from "../pg/pg.constants";
+import pg from "pg";
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(@Inject(PG_POOL) private readonly pgPool: pg.Pool) {}
+  async getHello(): Promise<string> {
+    return await this.selectVersion()
+  }
+
+  private async selectVersion(): Promise<string> {
+    const text = "select version()"
+    const r = await this.pgPool.query(text)
+    return Promise.resolve(JSON.stringify(r.rows[0].version))
   }
 }
