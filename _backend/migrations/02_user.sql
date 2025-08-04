f177431bd5927ba5f96927f0c3c06a89f6205293
Eric Le Ponner <eric.leponner@icloud.com>
2025-08-04 18:12:46 +0200

    Added user table.
    Signed-off-by: Eric Le Ponner <eric.leponner@icloud.com>

---
+++ _backend/migrations/02_user.sql
@@ -0,0 +1,17 @@
+CREATE TABLE "user"
+(
+    user_id           uuid PRIMARY KEY                          DEFAULT uuid_generate_v1mc(),
+
+    created_at        timestamptz                   NOT NULL    DEFAULT now(),
+    updated_at        timestamptz,
+    last_login_at     timestamptz,
+    last_seen_at      timestamptz,
+
+    email             text COLLATE case_insensitive NOT NULL    UNIQUE,
+
+    email_verified_at timestamptz,
+
+    password_hash     text
+);
+
+SELECT trigger_updated_at('"user"');
