c1fb729d825e22e82b6c250058dd2c11eee786f4
Eric Le Ponner <eric.leponner@icloud.com>
2025-08-05 09:52:13 +0200

    Tuned prettier and reformated code.
    Signed-off-by: Eric Le Ponner <eric.leponner@icloud.com>

--- _backend/src/app/app.controller.spec.ts
+++ _backend/src/app/app.controller.spec.ts
@@ -1,28 +1,28 @@
 // SPDX-License-Identifier: Apache-2.0
 
-import { beforeEach, describe, it } from 'node:test';
-import assert from 'node:assert';
-import { Test, TestingModule } from '@nestjs/testing';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import {PgModule} from "../pg/pg.module";
+import { beforeEach, describe, it } from "node:test"
+import assert from "node:assert"
+import { Test, TestingModule } from "@nestjs/testing"
+import { AppController } from "./app.controller"
+import { AppService } from "./app.service"
+import { PgModule } from "../pg/pg.module"
 
-describe('AppController', () => {
-  let appController: AppController;
+describe("AppController", () => {
+  let appController: AppController
 
   beforeEach(async () => {
     const app: TestingModule = await Test.createTestingModule({
       imports: [PgModule],
       controllers: [AppController],
       providers: [AppService],
-    }).compile();
+    }).compile()
 
-    appController = app.get<AppController>(AppController);
-  });
+    appController = app.get<AppController>(AppController)
+  })
 
-  describe('root', () => {
+  describe("root", () => {
     it('should return "Hello World!"', async () => {
-      assert.match(await appController.getHello(), /PostgreSQL/);
-    });
-  });
-});
+      assert.match(await appController.getHello(), /PostgreSQL/)
+    })
+  })
+})
--- _backend/src/app/app.module.ts
+++ _backend/src/app/app.module.ts
@@ -1,10 +1,10 @@
 // SPDX-License-Identifier: Apache-2.0
 
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { ConfigModule } from '@nestjs/config';
-import {PgModule} from "../pg/pg.module";
+import { Module } from "@nestjs/common"
+import { AppController } from "./app.controller"
+import { AppService } from "./app.service"
+import { ConfigModule } from "@nestjs/config"
+import { PgModule } from "../pg/pg.module"
 
 @Module({
   imports: [ConfigModule.forRoot(), PgModule],
