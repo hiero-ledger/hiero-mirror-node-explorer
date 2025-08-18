4d3273c140107216e8deebbd620f34c59c26832a
Eric Le Ponner <eric.leponner@icloud.com>
2025-08-18 13:30:23 +0200

    Now calling NestApplication.enableCors().
    Signed-off-by: Eric Le Ponner <eric.leponner@icloud.com>

--- _backend/src/main.config.ts
+++ _backend/src/main.config.ts
@@ -7,4 +7,14 @@ export function mainConfig(app: INestApplication) {
   app.setGlobalPrefix("/api/v1")
   app.useGlobalPipes(new ValidationPipe())
   app.use(cookieParser())
+
+// https://github.com/expressjs/cors#configuration-options
+  app.enableCors({
+    origin: true,
+    credentials: true,
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+  });
+
 }
