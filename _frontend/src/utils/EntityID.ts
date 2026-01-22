52021d27432b0a38169c238d87c1f883a14b0439
Simon Viénot <simon.vienot@icloud.com>
2026-01-22 18:49:02 +0100

    Adjust EntityID.compareAccountID() to cope for 802 account.
    Signed-off-by: Simon Viénot <simon.vienot@icloud.com>

--- _frontend/src/utils/EntityID.ts
+++ _frontend/src/utils/EntityID.ts
@@ -112,10 +112,10 @@ export class EntityID {
             result = compareNumber(this.realm, that.realm)
         }
         if (result == 0) {
-            if ((this.num < 100 || this.num === 800 || this.num === 801) && (that.num >= 100 && that.num !== 800 && that.num !== 801)) {
+            if (this.isSpecialAccount() && !that.isSpecialAccount()) {
                 // We put this.num at the end
                 result = +1
-            } else if ((that.num < 100 || that.num === 800 || that.num === 801) && (this.num >= 100 && this.num != 800 && this.num != 801)) {
+            } else if (that.isSpecialAccount() && !this.isSpecialAccount()) {
                 // We put that.num at the end
                 result = -1
             } else {
@@ -137,6 +137,10 @@ export class EntityID {
         return result
     }
 
+    public isSpecialAccount(): boolean {
+        const feeAccounts = [800, 801, 802]
+        return (this.num < 100 || feeAccounts.includes(this.num))
+    }
 
     // Utility
 
