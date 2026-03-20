13137e3312e6f27bd729365923ccb3215d072d0d
Simon Viénot <simon.vienot@icloud.com>
2026-03-20 14:28:46 +0100

    Display place-holder in account hooks section when inactive EVM address.
    Signed-off-by: Simon Viénot <simon.vienot@icloud.com>

--- _frontend/src/pages/AccountDetails_Hooks.vue
+++ _frontend/src/pages/AccountDetails_Hooks.vue
@@ -6,7 +6,17 @@
 
 <template>
 
-  <HooksSection :account-id="normalizedAccountId ?? undefined"/>
+  <template v-if="normalizedAccountId">
+    <HooksSection :account-id="normalizedAccountId"/>
+  </template>
+  <template v-else>
+    <DashboardCardV2 v-if="accountId">
+      <template #title>Hiero Hooks</template>
+      <template #content>
+        <DocSnippet><p>Hooks cannot be configured for this account because it is not yet activated.</p></DocSnippet>
+      </template>
+    </DashboardCardV2>
+  </template>
 
 </template>
 
@@ -20,6 +30,8 @@ import {computed, onBeforeUnmount, onMounted} from 'vue';
 import {AccountLocParser} from "@/utils/parser/AccountLocParser";
 import {NetworkConfig} from "@/config/NetworkConfig";
 import HooksSection from "@/components/hooks/HooksSection.vue";
+import DocSnippet from "@/components/DocSnippet.vue";
+import DashboardCardV2 from "@/components/DashboardCardV2.vue";
 
 const props = defineProps({
   accountId: String,
