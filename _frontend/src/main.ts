71a844c876edab30853067308483699c55e72a94
Simon Viénot <simon.vienot@icloud.com>
2026-04-20 09:38:34 +0200

    chore: bump dependencies (#2402)
    Signed-off-by: Simon Viénot <simon.vienot@icloud.com>

--- _frontend/src/main.ts
+++ _frontend/src/main.ts
@@ -8,6 +8,7 @@ import {FontAwesomeIcon} from "@fortawesome/vue-fontawesome";
 import {library} from "@fortawesome/fontawesome-svg-core";
 import {faForward} from "@fortawesome/free-solid-svg-icons";
 
+import "@oruga-ui/theme-oruga/dist/oruga.css";
 import "@/styles/explorer.css";
 import {AxiosMonitor} from "@/utils/AxiosMonitor";
 import {CoreConfig} from "@/config/CoreConfig";
