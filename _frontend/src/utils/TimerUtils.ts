c17a5031c5c61dbbe4445959b00bab284acc410a
Eric Le Ponner <eric.leponner@icloud.com>
2026-04-28 09:12:00 +0200

    Added abortableWaitFor() (to be used later)
    Signed-off-by: Eric Le Ponner <eric.leponner@icloud.com>

--- _frontend/src/utils/TimerUtils.ts
+++ _frontend/src/utils/TimerUtils.ts
@@ -4,3 +4,22 @@
 export async function waitFor(milliseconds: number): Promise<void> {
     return new Promise(resolve => setTimeout(resolve, milliseconds))
 }
+
+export async function abortableWaitFor(milliseconds: number, signal: AbortSignal): Promise<void> {
+    return new Promise(resolve => {
+        if (signal.aborted) {
+            resolve()
+        } else {
+            const handleTimeout = () => {
+                signal.removeEventListener( "abort", handleAbort );
+                resolve()
+            }
+            const handleAbort = () => {
+                clearTimeout(timeoutID);
+                handleTimeout()
+            }
+            const timeoutID = setTimeout(handleTimeout, milliseconds)
+            signal.addEventListener( "abort", handleAbort);
+        }
+    })
+}
