b0410a1e6d12059eff479b26706e9b213cba9850
Simon Viénot <simon.vienot@icloud.com>
2026-04-28 14:32:05 +0200

    Add empty ContractVerificationInfoDialog component.
    Signed-off-by: Simon Viénot <simon.vienot@icloud.com>

---
+++ _frontend/src/dialogs/verification/ContractVerificationInfoDialog.vue
@@ -0,0 +1,52 @@
+// SPDX-License-Identifier: Apache-2.0
+
+<!-- --------------------------------------------------------------------------------------------------------------- -->
+<!--                                                     TEMPLATE                                                    -->
+<!-- --------------------------------------------------------------------------------------------------------------- -->
+
+<template>
+  <ModalDialog v-model:show-dialog="showDialog" :width="600">
+
+    <template #modalDialogTitle>Verify Contract</template>
+
+    <template #modalDialogContent>
+
+
+    </template>
+
+    <template #modalDialogButtons>
+      <ModalDialogButton v-model:show-dialog="showDialog">CLOSE</ModalDialogButton>
+    </template>
+
+  </ModalDialog>
+</template>
+
+<!-- --------------------------------------------------------------------------------------------------------------- -->
+<!--                                                      SCRIPT                                                     -->
+<!-- --------------------------------------------------------------------------------------------------------------- -->
+
+<script lang="ts" setup>
+
+import ModalDialog from "@/dialogs/core/ModalDialog.vue";
+import ModalDialogButton from "@/dialogs/core/ModalDialogButton.vue";
+import {PropType} from "vue";
+
+const props = defineProps({
+  contractId: {
+    type: String as PropType<string | null>,
+    default: null
+  }
+})
+
+const showDialog = defineModel("showDialog", {
+  type: Boolean,
+  required: true
+})
+
+</script>
+
+<!-- --------------------------------------------------------------------------------------------------------------- -->
+<!--                                                       STYLE                                                     -->
+<!-- --------------------------------------------------------------------------------------------------------------- -->
+
+<style/>
