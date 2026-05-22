d01ed1ee80569278360c646d01232305f4b9f0f6
Simon Viénot <simon.vienot@icloud.com>
2026-05-22 16:26:07 +0200

    Fix layout of components of compact TransferGraphSection.
    Signed-off-by: Simon Viénot <simon.vienot@icloud.com>

--- _frontend/src/components/transfer_graphs/HbarTransferGraphC.vue
+++ _frontend/src/components/transfer_graphs/HbarTransferGraphC.vue
@@ -6,52 +6,54 @@
 
 <template>
 
-  <div v-if="hbarTransferLayout.rowCount >= 1" class="graph-container">
+  <div>
+    <div v-if="hbarTransferLayout.rowCount >= 1" class="graph-container">
 
-    <template v-for="i in hbarTransferLayout.rowCount" v-bind:key="i">
+      <template v-for="i in hbarTransferLayout.rowCount" v-bind:key="i">
 
-      <!-- #0 : account id -->
-      <div>
-        <AccountLink v-if="i <= hbarTransferLayout.sources.length"
-                     v-bind:account-id="hbarTransferLayout.sources[i-1].transfer.account"
-                     v-bind:no-anchor="true"
-                     null-label="MINT"
-                     data-cy="sourceAccount"/>
-      </div>
+        <!-- #0 : account id -->
+        <div>
+          <AccountLink v-if="i <= hbarTransferLayout.sources.length"
+                       v-bind:account-id="hbarTransferLayout.sources[i-1].transfer.account"
+                       v-bind:no-anchor="true"
+                       null-label="MINT"
+                       data-cy="sourceAccount"/>
+        </div>
 
-      <!-- #1 : arrow -->
-      <div style="position: relative">
-        <ArrowSegment
-            v-bind:source-count="hbarTransferLayout.sources.length"
-            v-bind:compact="true"
-            v-bind:row-index="i-1"/>
-      </div>
+        <!-- #1 : arrow -->
+        <div style="position: relative">
+          <ArrowSegment
+              v-bind:source-count="hbarTransferLayout.sources.length"
+              v-bind:compact="true"
+              v-bind:row-index="i-1"/>
+        </div>
 
-      <!-- #2 : hbar amount -->
-      <div class="justify-end">
-        <HbarAmount v-if="i === 1"
-                    v-bind:amount="hbarTransferLayout.destinationAmount"/>
-      </div>
+        <!-- #2 : hbar amount -->
+        <div class="justify-end">
+          <HbarAmount v-if="i === 1"
+                      v-bind:amount="hbarTransferLayout.destinationAmount"/>
+        </div>
 
-      <!-- #3 : arrow -->
-      <div style="position: relative">
-        <ArrowSegment
-            v-bind:dest-count="hbarTransferLayout.destinations.length"
-            v-bind:compact="true"
-            v-bind:row-index="i-1"/>
-      </div>
+        <!-- #3 : arrow -->
+        <div style="position: relative">
+          <ArrowSegment
+              v-bind:dest-count="hbarTransferLayout.destinations.length"
+              v-bind:compact="true"
+              v-bind:row-index="i-1"/>
+        </div>
 
-      <!-- #4 : account id -->
-      <div>
-        <AccountLink v-if="i <= hbarTransferLayout.destinations.length"
-                     v-bind:account-id="hbarTransferLayout.destinations[i-1].transfer.account"
-                     v-bind:no-anchor="true"
-                     null-label="BURN"
-                     data-cy="destinationAccount"/>
-      </div>
+        <!-- #4 : account id -->
+        <div>
+          <AccountLink v-if="i <= hbarTransferLayout.destinations.length"
+                       v-bind:account-id="hbarTransferLayout.destinations[i-1].transfer.account"
+                       v-bind:no-anchor="true"
+                       null-label="BURN"
+                       data-cy="destinationAccount"/>
+        </div>
 
-    </template>
+      </template>
 
+    </div>
   </div>
 
 </template>
