117eaf501abc9692dda682e8cd08ccf9e7674a15
Simon Viénot <simon.vienot@icloud.com>
2026-06-09 16:33:37 +0200

    Add AccessListSection component (for further use).
    Signed-off-by: Simon Viénot <simon.vienot@icloud.com>

---
+++ _frontend/src/components/contract/AccessListSection.vue
@@ -0,0 +1,63 @@
+// SPDX-License-Identifier: Apache-2.0
+
+<!-- --------------------------------------------------------------------------------------------------------------- -->
+<!--                                                     TEMPLATE                                                    -->
+<!-- --------------------------------------------------------------------------------------------------------------- -->
+
+<template>
+
+  <DashboardCardV2>
+    <template #title>
+      <span>Access List</span>
+    </template>
+    <template #content>
+      <div class="access-list">
+        <template v-for="(item, index) in props.accessList" :key="index">
+          <AccessListItemView :item="item"/>
+          <hr class="table-separator"/>
+        </template>
+      </div>
+    </template>
+  </DashboardCardV2>
+
+</template>
+
+<!-- --------------------------------------------------------------------------------------------------------------- -->
+<!--                                                      SCRIPT                                                     -->
+<!-- --------------------------------------------------------------------------------------------------------------- -->
+
+<script lang="ts" setup>
+
+import {PropType} from "vue";
+import {AccessListItem} from "@/schemas/MirrorNodeSchemas.ts";
+import DashboardCardV2 from "@/components/DashboardCardV2.vue";
+import AccessListItemView from "@/components/contract/AccessListItemView.vue";
+
+const props = defineProps({
+  accessList: {
+    type: Array as PropType<Array<AccessListItem>>,
+    required: true,
+  },
+})
+
+</script>
+
+<!-- --------------------------------------------------------------------------------------------------------------- -->
+<!--                                                      STYLE                                                      -->
+<!-- --------------------------------------------------------------------------------------------------------------- -->
+
+<style scoped>
+
+hr.table-separator {
+  border-color: var(--border-secondary);
+  border-width: 0.5px;
+  margin: 0;
+}
+
+div.access-list {
+  display: flex;
+  flex-direction: column;
+  gap: 16px;
+}
+
+</style>
