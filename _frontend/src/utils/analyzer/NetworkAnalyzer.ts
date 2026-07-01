f3c4dc22472c23aa9c89b03bb96c462588e1fbb1
Simon Viénot <simon.vienot@icloud.com>
2026-07-01 20:07:28 +0200

    Only use RegisteredNodeCache and remove now unused derived caches.
    Signed-off-by: Simon Viénot <simon.vienot@icloud.com>

--- _frontend/src/utils/analyzer/NetworkAnalyzer.ts
+++ _frontend/src/utils/analyzer/NetworkAnalyzer.ts
@@ -3,22 +3,16 @@
 import {computed, ComputedRef, ref, Ref, watch, WatchStopHandle} from "vue";
 import {StakingPeriod} from "@/utils/StakingPeriod";
 import {NodeCache} from "@/utils/cache/NodeCache.ts";
-import {BlockNodeCache} from "@/utils/cache/BlockNodeCache.ts";
-import {MirrorNodeCache} from "@/utils/cache/MirrorNodeCache.ts";
-import {RpcRelayCache} from "@/utils/cache/RpcRelayCache.ts";
 import {SingletonLookup} from "@/utils/cache/base/SingletonCache";
-import {NetworkNode, RegisteredNode} from "@/schemas/MirrorNodeSchemas";
-import {GeneralServiceCache} from "../cache/GeneralServiceCache.ts";
+import {NetworkNode, RegisteredNode, RegisteredNodeType} from "@/schemas/MirrorNodeSchemas";
+import {RegisteredNodeCache} from "@/utils/cache/RegisteredNodeCache.ts";
 
 
 export class NetworkAnalyzer {
 
     public readonly nodeLookup: SingletonLookup<NetworkNode[]> = NodeCache.instance.makeLookup()
 
-    public readonly blockNodeLookup: SingletonLookup<RegisteredNode[]> = BlockNodeCache.instance.makeLookup()
-    public readonly mirrorNodeLookup: SingletonLookup<RegisteredNode[]> = MirrorNodeCache.instance.makeLookup()
-    public readonly rpcRelayLookup: SingletonLookup<RegisteredNode[]> = RpcRelayCache.instance.makeLookup()
-    public readonly generalServiceLookup: SingletonLookup<RegisteredNode[]> = GeneralServiceCache.instance.makeLookup()
+    public readonly registeredNodeLookup: SingletonLookup<RegisteredNode[]> = RegisteredNodeCache.instance.makeLookup()
 
     public readonly stakingPeriod: Ref<StakingPeriod | null> = ref(null)
     private intervalHandle = -1
@@ -27,32 +21,6 @@ export class NetworkAnalyzer {
     //
     // Public
     //
-
-    public mount(): void {
-        this.nodeLookup.mount()
-        this.blockNodeLookup.mount()
-        this.mirrorNodeLookup.mount()
-        this.rpcRelayLookup.mount()
-        this.generalServiceLookup.mount()
-        this.updateStakingPeriod()
-        this.intervalHandle = window.setInterval(this.updateStakingPeriod, 10000)
-        this.watchHandle = watch(this.nodes, this.updateStakingPeriod)
-    }
-
-    public unmount(): void {
-        this.nodeLookup.unmount()
-        this.blockNodeLookup.unmount()
-        this.mirrorNodeLookup.unmount()
-        this.rpcRelayLookup.unmount()
-        this.generalServiceLookup.unmount()
-        this.stakingPeriod.value = null
-        window.clearInterval(this.intervalHandle)
-        this.intervalHandle = -1
-        if (this.watchHandle !== null) {
-            this.watchHandle()
-            this.watchHandle = null
-        }
-    }
 
     public readonly nodes = computed(() => this.nodeLookup.entity.value ?? [])
 
@@ -121,14 +89,56 @@ export class NetworkAnalyzer {
     public readonly remainingMin
         = computed(() => this.stakingPeriod.value?.remainingMin ?? null)
 
-    public readonly blockNodes = computed(() => this.blockNodeLookup.entity.value ?? [])
-    public readonly mirrorNodes = computed(() => this.mirrorNodeLookup.entity.value ?? [])
-    public readonly rpcRelays = computed(() => this.rpcRelayLookup.entity.value ?? [])
-    public readonly generalServices = computed(() => this.generalServiceLookup.entity.value ?? [])
+    public readonly registeredNodes = computed(() => this.registeredNodeLookup.entity.value ?? [])
+    private readonly registeredNodesByRole = computed(() => {
+        const blockNodes: RegisteredNode[] = []
+        const mirrorNodes: RegisteredNode[] = []
+        const rpcRelays: RegisteredNode[] = []
+        const generalServices: RegisteredNode[] = []
+
+        for (const n of this.registeredNodes.value) {
+            for (const e of n.service_endpoints) {
+                if (e.type === RegisteredNodeType.BLOCK_NODE) {
+                    blockNodes.push(n)
+                } else if (e.type === RegisteredNodeType.MIRROR_NODE) {
+                    mirrorNodes.push(n)
+                } else if (e.type === RegisteredNodeType.RPC_RELAY) {
+                    rpcRelays.push(n)
+                } else {
+                    generalServices.push(n)
+                }
+            }
+        }
+        return {blockNodes, mirrorNodes, rpcRelays, generalServices}
+    })
+    public readonly blockNodes = computed(() => this.registeredNodesByRole.value.blockNodes)
+    public readonly mirrorNodes = computed(() => this.registeredNodesByRole.value.mirrorNodes)
+    public readonly rpcRelays = computed(() => this.registeredNodesByRole.value.rpcRelays)
+    public readonly generalServices = computed(() => this.registeredNodesByRole.value.generalServices)
+
+    public mount(): void {
+        this.nodeLookup.mount()
+        this.registeredNodeLookup.mount()
+        this.updateStakingPeriod()
+        this.intervalHandle = window.setInterval(this.updateStakingPeriod, 10000)
+        this.watchHandle = watch(this.nodes, this.updateStakingPeriod)
+    }
 
     //
     // Private
     //
+
+    public unmount(): void {
+        this.nodeLookup.unmount()
+        this.registeredNodeLookup.unmount()
+        this.stakingPeriod.value = null
+        window.clearInterval(this.intervalHandle)
+        this.intervalHandle = -1
+        if (this.watchHandle !== null) {
+            this.watchHandle()
+            this.watchHandle = null
+        }
+    }
 
     private readonly updateStakingPeriod = () => {
         let startTimeInSec, endTimeInSec
--- _frontend/src/utils/cache/RegisteredNodeCache.ts
+++ _frontend/src/utils/cache/RegisteredNodeCache.ts
@@ -1,6 +1,12 @@
 // SPDX-License-Identifier: Apache-2.0
 
-import {RegisteredNode, RegisteredNodesResponse, RegisteredNodeType} from "@/schemas/MirrorNodeSchemas";
+import {
+    KeyType,
+    RegisteredBlockNodeApi,
+    RegisteredNode,
+    RegisteredNodesResponse,
+    RegisteredNodeType
+} from "@/schemas/MirrorNodeSchemas";
 import {SingletonCache} from "@/utils/cache/base/SingletonCache";
 import axios, {AxiosResponse} from "axios";
 
@@ -8,15 +14,168 @@ const mockRegisteredNodes = import.meta.env.VITE_APP_MOCK_HIP_1137 === "true"
 
 export class RegisteredNodeCache extends SingletonCache<RegisteredNode[]> {
 
-    private nodeType: RegisteredNodeType
+    public static readonly instance = new RegisteredNodeCache()
 
-    protected constructor(nodeType: RegisteredNodeType) {
-        super()
-        this.nodeType = nodeType
-    }
-
+    // eslint-disable-next-line max-lines-per-function
     protected getMock(): RegisteredNode[] {
-        return []
+        return [
+            {
+                admin_key: {
+                    "_type": KeyType.ED25519,
+                    "key": "d6e8334cd8594e88c82ff266b4974b4e4ac596962dcfab7314f935e7fdda672f"
+                },
+                created_timestamp: "1648377044.798291252",
+                description: "Sample Registered Node (1 block node endpoint) ",
+                registered_node_id: 1,
+                service_endpoints: [
+                    {
+                        block_node: {
+                            endpoint_apis: [RegisteredBlockNodeApi.STATE_PROOF]
+                        },
+                        domain_name: "block1.alpha.com",
+                        general_service: null,
+                        ip_address: "192.168.12.42",
+                        mirror_node: null,
+                        port: 40840,
+                        requires_tls: true,
+                        rpc_relay: null,
+                        type: RegisteredNodeType.BLOCK_NODE
+                    },
+                ],
+                timestamp: {"from": "1648377044.798291252", "to": null}
+            },
+            {
+                admin_key: {
+                    "_type": KeyType.ED25519,
+                    "key": "d6e8334cd8594e88c82ff266b4974b4e4ac596962dcfab7314f935e7fdda672f"
+                },
+                created_timestamp: "1648377044.798291252",
+                description: "Sample Registered Node (1 mirror node endpoint) ",
+                registered_node_id: 2,
+                service_endpoints: [
+                    {
+                        block_node: null,
+                        domain_name: "mirror1.alpha.com",
+                        general_service: null,
+                        ip_address: "192.168.12.42",
+                        mirror_node: null,
+                        port: 40840,
+                        requires_tls: true,
+                        rpc_relay: null,
+                        type: RegisteredNodeType.MIRROR_NODE
+                    },
+                ],
+                timestamp: {"from": "1648377044.798291252", "to": null}
+            },
+            {
+                admin_key: {
+                    "_type": KeyType.ED25519,
+                    "key": "d6e8334cd8594e88c82ff266b4974b4e4ac596962dcfab7314f935e7fdda672f"
+                },
+                created_timestamp: "1648377044.798291252",
+                description: "Sample Registered Node (1 RPC relay endpoint) ",
+                registered_node_id: 3,
+                service_endpoints: [
+                    {
+                        block_node: null,
+                        domain_name: "relay1.alpha.com",
+                        general_service: null,
+                        ip_address: "192.168.12.42",
+                        mirror_node: null,
+                        port: 40840,
+                        requires_tls: true,
+                        rpc_relay: null,
+                        type: RegisteredNodeType.RPC_RELAY
+                    },
+                ],
+                timestamp: {"from": "1648377044.798291252", "to": null}
+            },
+            {
+                admin_key: {
+                    "_type": KeyType.ED25519,
+                    "key": "d6e8334cd8594e88c82ff266b4974b4e4ac596962dcfab7314f935e7fdda672f"
+                },
+                created_timestamp: "1648377044.798291252",
+                description: "Sample Registered Node (1 general service endpoint) ",
+                registered_node_id: 4,
+                service_endpoints: [
+                    {
+                        block_node: null,
+                        domain_name: null,
+                        general_service: {
+                            description: "This is a mocked-up general service",
+                        },
+                        ip_address: "192.168.12.12",
+                        mirror_node: null,
+                        port: 40840,
+                        requires_tls: true,
+                        rpc_relay: null,
+                        type: RegisteredNodeType.GENERAL_SERVICE
+                    },
+                ],
+                timestamp: {"from": "1648377044.798291252", "to": null}
+            },
+            {
+                admin_key: {
+                    "_type": KeyType.ED25519,
+                    "key": "d6e8334cd8594e88c82ff266b4974b4e4ac596962dcfab7314f935e7fdda672f"
+                },
+                created_timestamp: "1648377044.798291252",
+                description: "Sample Registered Node (4 service endpoints, 1 of each type) ",
+                registered_node_id: 10,
+                service_endpoints: [
+                    {
+                        block_node: {
+                            endpoint_apis: [RegisteredBlockNodeApi.STATE_PROOF]
+                        },
+                        domain_name: "block3.alpha.com",
+                        general_service: null,
+                        ip_address: null,
+                        mirror_node: null,
+                        port: 40840,
+                        requires_tls: true,
+                        rpc_relay: null,
+                        type: RegisteredNodeType.BLOCK_NODE
+                    },
+                    {
+                        block_node: null,
+                        domain_name: "mirror2.alpha.com",
+                        general_service: null,
+                        ip_address: null,
+                        mirror_node: null,
+                        port: 40840,
+                        requires_tls: false,
+                        rpc_relay: null,
+                        type: RegisteredNodeType.MIRROR_NODE
+                    },
+                    {
+                        block_node: null,
+                        domain_name: null,
+                        general_service: null,
+                        ip_address: "192.168.42.42",
+                        mirror_node: null,
+                        port: 40840,
+                        requires_tls: false,
+                        rpc_relay: null,
+                        type: RegisteredNodeType.RPC_RELAY
+                    },
+                    {
+                        block_node: null,
+                        domain_name: "general.service.com",
+                        general_service: {
+                            description: "This is another mocked-up general service",
+                        },
+                        ip_address: null,
+                        mirror_node: null,
+                        port: 40840,
+                        requires_tls: true,
+                        rpc_relay: null,
+                        type: RegisteredNodeType.GENERAL_SERVICE
+                    }
+                ],
+                timestamp: {"from": "1648377044.798291252", "to": null}
+            },
+        ]
     }
 
     //
@@ -33,7 +192,6 @@ export class RegisteredNodeCache extends SingletonCache<RegisteredNode[]> {
             let nextURL: string | null = "api/v1/network/registered-nodes"
             const params = {
                 limit: 100 as number | undefined,
-                type: this.nodeType
             }
             while (nextURL !== null) {
                 const response: AxiosResponse<RegisteredNodesResponse>
